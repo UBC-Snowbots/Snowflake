fe564000768fbbb69caa9f42585d118ae822f7da
Gareth Ellis <gareth.ellis0@gmail.com>
2016-07-14 18:19:00 -0700

    Added installation script for ROS and CLion IDE

---
+++ install_required.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+###############################
+# Install ROS
+###############################
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB01FA116
+sudo apt-get update
+sudo apt-get install ros-kinetic-desktop-full
+
+# Initialize rosdep
+sudo rosdep init
+rosdep update
+
+# Source ROS Environment Variables Automatically
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+# rosinstall allows downloading of source trees for ROS packages 
+# with a single command
+sudo apt-get install python-rosinstall
+
+
+###############################
+# Install CLion
+###############################
+install by executing `./install.sh`.
+Raw  install_clion.sh
+#!/usr/bin/env bash
+
+# Install dependencies
+sudo apt-get install -y openjdk-7-jdk git
+
+# Fetch and extract CLion
+echo "Fetching and extracting CLion"
+wget https://download.jetbrains.com/cpp/CLion-2016.1.3.tar.gz
+tar xzf CLion*.tar.gz -C ~
+rm CLion*.tar.gz
+
+# Run CLion Setup
+cd ~/clion*
+./bin/clion.sh
+
+# Make CLion globally accessible
+echo "Linking CLion"
+sudo ln -s "$(pwd)/bin/clion.sh" "/usr/local/bin/clion"
+
