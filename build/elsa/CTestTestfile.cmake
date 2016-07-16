05a146183867afb1839737390486946b78b33dd8
Gareth Ellis <gareth.ellis0@gmail.com>
2016-07-15 19:11:59 -0700

    Setup basic ROS project

---
+++ build/elsa/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/gareth/programming/IGVC-2017/src/elsa
+# Build directory: /home/gareth/programming/IGVC-2017/build/elsa
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
