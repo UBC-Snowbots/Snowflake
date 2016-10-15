701b80d777f3fa3381b9872950053d8210d9a86b
James Asefa <jamesasefa@gmail.com>
2016-10-15 11:15:17 -0700

    early re-factoring of drivetrain firmware

--- src/firmware/main.cpp
+++ src/firmware/main.cpp
@@ -1,8 +1,2 @@
 #include <iostream>
-
-using namespace std;
-
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
-}
+using namespace std;int main(){cout<<"Hello, World!"<<endl;return 0;}
