d91b04f436aa3e3151e44f0b4fdf9e3d3886030e
Gareth Ellis <gareth.ellis0@gmail.com>
2016-10-20 16:33:15 -0700

    Minor type fix in publisher initialization

--- src/decision/src/GpsManager.cpp
+++ src/decision/src/GpsManager.cpp
@@ -12,14 +12,14 @@ GpsManager::GpsManager(int argc, char **argv, std::string node_name){
 
     // Setup Subscribers
     std::string raw_gps_topic_name = "/gps_driver/raw_gps";
-    int refresh_rate = 10;
+    uint32_t refresh_rate = 10;
     raw_gps_subscriber = public_nh.subscribe(raw_gps_topic_name, refresh_rate,
                                              &GpsManager::rawGpsCallBack, this);
 
     // Setup Publishers
     std::string compass_heading_topic = public_nh.resolveName("current_heading");
     uint32_t queue_size = 10;
-    compass_heading_publisher = nh.advertise<std_msgs::Float32_>(compass_heading_topic, queue_size);
+    compass_heading_publisher = nh.advertise<std_msgs::Float32>(compass_heading_topic, queue_size);
 }
 
 void GpsManager::rawGpsCallBack(const sensor_msgs::NavSatFix::ConstPtr nav_sat_fix) {
