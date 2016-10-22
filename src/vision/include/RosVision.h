ecd4bfc616b9f20e5b0485ba8748e06ab7b8b177
ValRat <leri.ratu@gmail.com>
2016-10-22 12:42:29 -0700

    Vision port (#17)
    * Vision port from old repo
    
    * Added docs
    
    * Vision refactor into class
    
    * Readme update (#18)
    
    * Removed stray include
    
    * Added warning about adding project crucial dependencies to install script (#21)
    
    * Implemented CI (Continuous Integration) (#22)
    
    * Initial travis setup testing
    
    * Fixed indenting
    
    * Added rosinstall file
    
    * CMake spelling fix to allow steering-driver-tests to compile
    
    * Added build passing/failing image to readme
    
    * Travis now (hopefully) pulls the rosinstall file from the current branch
    
    * Missed a static branch reference in the last commit
    
    * Removed myself from the notification lists (#23)
    
    Because I'd rather *not* get emailed every time *ANYTHING* happens with travis ci
    
    * Readme fixes (#26)
    
    * Minor phrasing fix
    
    * Brief and indentation convention added
    
    * Cleaned up documentation and renamed file
    
    * Removed unused feature
    
    * Vision port from old repo
    
    * Added docs
    
    * Vision refactor into class
    
    * Removed stray include
    
    * Cleaned up documentation and renamed file
    
    * Removed unused feature
    
    * attempted fix for adding opencv as a rosdep
    
    * another attempted fix to rosdep opencv issues
    
    * fixes for rosdep opencv3

---
+++ src/vision/include/RosVision.h
@@ -0,0 +1,96 @@
+/*
+ * Created by: Valerian Ratu
+ * Created On: October 15, 2016
+ * Description: Subscribes to image from a camera and publishes a binary image
+ *              depending on the filter specification and setting
+ * Usage: Press m to calibrate filter values, press m to save it.
+ *
+ * Subscribes to: /else/camera/image_raw
+ * Publishes to: /vision/filtered_image
+ *
+ */
+
+
+#ifndef PROJECT_ROSVISION_H_H
+#define PROJECT_ROSVISION_H_H
+
+#include <ros/ros.h>
+#include <opencv2/core/core.hpp>
+#include <stdio.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+#include <opencv2/imgproc/imgproc.hpp>
+#include "filter.h"
+#include "IPM.h"
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <ros/console.h>
+#include <ros/time.h>
+
+class RosVision {
+
+public:
+
+    /**
+     * Constructor
+     */
+    RosVision(int argc, char** argv, std::string node_name);
+
+    RosVision();
+
+private:
+
+    /**
+     * Callback executes on subscribed images, each callback will analyze and output a binary image
+     *
+     * @param msg the image message subscribed to
+     */
+    void imageCallback(const sensor_msgs::ImageConstPtr& msg);
+
+    /**
+     * Creates windows showing the image at various points in the pipeline
+     */
+    void createWindow();
+
+    /**
+     * Destroys windows
+     */
+    void deleteWindow();
+
+    //Publisher and subscribers
+    image_transport::Subscriber sub;
+    image_transport::Publisher pub;
+    std::string image_topic;
+    std::string output_topic;
+
+    //Frequency handling
+    ros::Time last_published;
+    ros::Duration publish_interval;
+
+    //Image processing Mat pipeline
+    cv::Mat inputImage;
+    cv::Mat workingImage;
+    cv::Mat ipmOutput;
+    cv::Mat filterOutput;
+
+    //Display window string names
+    std::string inputWindow;
+    std::string ipmOutputWindow;
+    std::string filterOutputWindow;
+
+    //Filters and their variables
+    snowbotsFilter filter;
+    IPM ipm;
+    int width , height;
+    int x1, x2, x3, x4, y1, y2, y3, y4;
+    std::vector<cv::Point2f> orig_points;
+    std::vector<cv::Point2f> dst_points;
+
+    //Debug and calibration variables
+    bool showWindow;
+    bool isCalibratingManually;
+};
+
+
+
+#endif //PROJECT_ROSVISION_H_H
