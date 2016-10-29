7cc8cd2c4f76cb8f9e41b6ca8cce866b5581b4ff
James Asefa <jamesasefa@gmail.com>
2016-10-29 13:19:38 -0700

    light testing of RC firmware. Need to check wiring. Steering calculation off

--- src/firmware/RC_firmware/RC_firmware.ino
+++ src/firmware/RC_firmware/RC_firmware.ino
@@ -1,157 +1,238 @@
 /* Firmware for Elsa
    Test version
    Author: Vincent Yuan, Modified: Nick Wu
-   Date: May 21, 2016
+     Modified: James Asefa
+   Date: Oct 30, 2016
 */
 
 #include <SoftwareSerial.h>
 #include <stdlib.h>
 #include <Servo.h>
 #include <math.h>
-#define TRIM 8
 
+#define TRIM 8
+#define BAUD_RATE 115200
+#define WIDTH 10
+#define BUFFER_SIZE 6
+// assign 128 to be not moving
+// will be used to hold robot in place while
+// reading from the buffer. Robot will be stopped initially.
+#define UNMAPPED_STOP_SPEED 128
 
 // DRIVING MODIFICATIONS
 // modify FR, FL, BR, BL to change speeds: (+) correpsonds to faster speed, (-) for going reverse
 // F:FRONT, B:BACK, R:RIGHT, L:LEFT e.g. FR = FRONT RIGHT WHEEL.
-const int OFFSET = 1, FR = 9, FL = 9, BR = 9, BL = 9;
+const int OFFSET = 1;
 
-Servo LeftMF, LeftMB, RightMB, RightMF;
+// buffer inputs
+int linear_x = 0;
+int linear_y = 0;
+int linear_z = 0;
+int angular_x = 0;
+int angular_y = 0;
+double angular_z = 0.0;
 
-int lx = 0, ly = 0, az = 0;
+// motor pins
+const int left_motor_pin = 9;
+const int right_motor_pin = 10;
+// defines start of buffer
+const char buffer_head = 'B';
+
+
+// max and min linear speeds and stopping condition
+const int linear_max = 100;
+const int linear_min = 80;
+const int linear_stop = 90;
+
+// max and min angular speeds and stopping condition
+const double angular_max = M_PI / 2;
+const double angular_min = - M_PI / 2;
+const double angular_stop = 0;
+
+Servo LeftM;
+Servo RightM;
+
+
+// ??
 int R1 = 0, R2 = 0, R3 = 0, R4 = 0, B2 = 0, B4 = 0, Mode = 0;
-int lx_h = 0, lx_l = 0, az_h = 0, az_l = 0;
+
+int linear_x_h = 0, linear_x_l = 0, angular_z_h = 0, angular_z_l = 0;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(BAUD_RATE);
+  
   pinMode(2, INPUT);
   pinMode(3, INPUT);
   pinMode(4, INPUT);
   pinMode(5, INPUT);
   
-  LeftMB.attach(10);
-  RightMF.attach(9);
-  
-  LeftMF.attach(12);
-  RightMB.attach(11);
+  LeftM.attach(left_motor_pin);
+  RightM.attach(right_motor_pin);
   
   set_off();
 }
 
 void loop() {
 
-  //if (Serial.read() == 'I'){Serial.print("DRIVE");}
+  //if (Serial.read() == 'I') {Serial.print("DRIVE");}
   //else Serial.flushRX();
   //responding to queries from usb
   sig_read();
-  Serial.println(Mode);
+  
+  /*Serial.println("Mode: ");Serial.println(Mode);Serial.println("R1: ");Serial.println(R1);
+  Serial.println("R2: ");
+  Serial.println(R2);
+  Serial.println("R3: ");
+  Serial.println(R3);*/
+  //Serial.println("linear_x_h: ");Serial.println(linear_x_h);Serial.println("linear_x_l: ");Serial.println(linear_x_l);Serial.println("angular_z_h: ");Serial.println(angular_z_h);
+  
+  
   //serial_read();
   if (Mode == -1) { //Auto Mode
-    serial_read(); lx = 128; ly = 128; az = B4;
+    serial_read(); 
+    
     convert();
-    drive();
+    drive(linear_x, angular_z);
+    Serial.print("angular_z_l: ");Serial.print(angular_z_l);Serial.print("linear_x: ");Serial.print(linear_x);Serial.print("angular_z: ");Serial.println(angular_z);
   }
   else if (Mode == 0) { //RC Mode
-    //Serial.println(az);
-    lx = 128; ly = R2; az = R1;
+    //Serial.println(angular_z);
+    
+    linear_x = R1; 
+    angular_z = R2;
+    
     convert();
-    drive();
+    drive(linear_x, angular_z);
+    Serial.print("angular_z_l: ");Serial.print(angular_z_l);Serial.print("linear_x: ");Serial.print(linear_x);Serial.print("angular_z: ");Serial.println(angular_z);
     Serial.flushRX();
   }
   else { //STOP MODE
-    lx = 128; ly = 128; az = 128;
+    linear_x = linear_stop; 
+    angular_z = angular_stop;
+    
     convert();
-    drive();
+    drive(linear_x, angular_z);
+    Serial.print("angular_z_l: ");Serial.print(angular_z_l);Serial.print("linear_x: ");Serial.print(linear_x);Serial.print("angular_z: ");Serial.println(angular_z);
     Serial.flushRX();
   }
-  //Serial.print("lx: ");Serial.print(lx);Serial.print(" ly: ");Serial.print(ly);Serial.print(" az: ");Serial.println(az);
+  //Serial.print("linear_x: ");Serial.print(linear_x);Serial.print(" linear_x: ");Serial.print(linear_x);Serial.print(" angular_z: ");Serial.println(angular_z);
 
 }
 
 void set_off() {
-  int lx_mid = 1550; //RX standard
-  int az_mid = 1550; //RY standard
-  lx_h = lx_mid + 150; lx_l = lx_mid - 150;
-  az_h = az_mid + 150; az_l = az_mid - 150;
+  int linear_x_mid = 1550; //RX standard
+  int angular_z_mid = 1550; //RY standard
+  
+  linear_x_h = linear_x_mid + 150; 
+  linear_x_l = linear_x_mid - 150;
+  
+  angular_z_h = angular_z_mid + 150; 
+  angular_z_l = angular_z_mid - 150;
 }
+
 
 void sig_read() {
-  R1 = pulseIn(2, HIGH); //1140 - 1965 RX LEFT-RIGHT
-  R2 = pulseIn(3, HIGH); //1965 - 1140 RY UP-DOWN
-  R3 = pulseIn(4, HIGH); //1970 - 1115 LY UP-DOWN
+  R1 = pulseIn(2, HIGH); // 1140 - 1965 RX LEFT-RIGHT -> turn on spot
+  R2 = pulseIn(3, HIGH); // 1965 - 1140 RY UP-DOWN -> Steer left/right y axis (turn while moving)
+  R3 = pulseIn(4, HIGH); // 1970 - 1115 linear_x UP-DOWN -> forward/backward
+  R4 = pulseIn(5,HIGH); //1970 - 1115 mode
   
-  //R4 = pulseIn(5,HIGH); //1970 - 1115 LX LEFT-RIGHT
-  //Serial.print("R1: "); Serial.print(R1);Serial.print(" R2: "); Serial.print(R2);Serial.print(" R3: "); Serial.println(R3);
-  if (R1 < lx_h && R1 > lx_l) R1 = 128;
-  else R1 = map (R1, 1140, 1965, 0, 255);
+  //Serial.print("R1: "); Serial.print(R1);Serial.print(" R2: "); Serial.print(R2);Serial.print(" R3: "); Serial.println(R3);Serial.println("Mode: ");Serial.println(Mode);
+  if (R1 < linear_x_h && R1 > linear_x_l) 
+    R1 = linear_stop;
+  else 
+    R1 = map (R1, 1140, 1965, 0, 255);
 
-  if (R2 < az_h && R2 > az_l) R2 = 128;
-  else R2 = map (R2, 1140, 1965, 0, 255);
+  if (R2 < angular_z_h && R2 > angular_z_l) 
+    R2 = angular_stop;
+  else 
+    R2 = map (R2, 1140, 1965, 0, 255);
   
   //Serial.print("R3: ");Serial.println(R3);
-  if (1100 < R3 && R3 < 1400) Mode = 1;
-  else if (1400 < R3 && R3 < 1700) Mode = 0;
-  else if (1700 < R3 && R3 < 2000) Mode = -1;
-  else Mode = -2;
+  if (1100 < R3 && R3 < 1400) 
+    Mode = 1;
+  else if (1400 < R3 && R3 < 1700) 
+    Mode = 0;
+  else if (1700 < R3 && R3 < 2000) 
+    Mode = -1;
+  else 
+    Mode = -2;
   //Serial.print("R1: "); Serial.println(R1);
   //Serial.print("R2: "); Serial.println(R2);
-  if (abs(R1 - 90) < TRIM) R1 = 128;
-  if (abs(R2 - 90) < TRIM) R2 = 128;
+  if (abs(R1 - 90) < TRIM) 
+    R1 = linear_stop;
+  if (abs(R2 - 90) < TRIM) 
+    R2 = angular_stop;
 }
 
-void serial_read() {
-  if (Serial.available() > 9) {
-    if (Serial.read() == 'B') {
-      int B9 = (Serial.read() - '0') * 100 + (Serial.read() - '0') * 10 + (Serial.read() - '0');
-      B2 = (Serial.read() - '0') * 100 + (Serial.read() - '0') * 10 + (Serial.read() - '0');
-      B4 = (Serial.read() - '0') * 100 + (Serial.read() - '0') * 10 + (Serial.read() - '0');
-      //Serial.print("B4: "); Serial.print(B4);
+void serial_read(){
+    //reading in 6 chars from Serial
+  if (Serial.available() > BUFFER_SIZE){
+
+      /* if (Serial.read() == 'B'){
+      linear_x = (Serial.read()-'0')*100 + (Serial.read()-'0')*10 + (Serial.read()-'0');
+      ly =(Serial.read()-'0')*100 + (Serial.read()-'0')*10 + (Serial.read()-'0');
+      angular_z = (Serial.read()-'0')*100 + (Serial.read()-'0')*10 + (Serial.read()-'0');*/
+
+      // buffer_head identifies the start of the buffer
+      if (Serial.read() == buffer_head) {
+          linear_x = Serial.read();
+          linear_y = Serial.read();
+          linear_z = Serial.read();
+          angular_x = Serial.read();
+          angular_y = Serial.read();
+          angular_z = (double) Serial.read();
+      } else {
+          linear_x = angular_z = UNMAPPED_STOP_SPEED;
+      }
+
+  } else {
+      linear_x = angular_z = UNMAPPED_STOP_SPEED;
     }
-    else {
-      B2 = B4 = 128;
-    }
-    //B2 = 128;
-  }
-  else {
-    B2 = B4 = 128;
-  }
+    
   //Serial.end();
   //Serial.begin(9600);
+  
+  //flushRX defined here: https://forum.sparkfun.com/viewtopic.php?f=32&t=32715 
   Serial.flushRX();
 }
 
 void convert() {
-  if (ly > 255)ly = 255; else if (ly < 0)ly = 0;
-  if (az > 255)az = 255; else if (az < 0)az = 0;
-  lx = map (lx, 0, 255, 80, 100);//80 100
-  ly = map (ly, 0, 255, 85, 95);//85 95
-  az = map (az, 0, 255, 80, 100);//80 100
+  if (linear_x > 255)
+    linear_x = 255; 
+  else if (linear_x < 0)
+    linear_x = 0;
+  
+  if (angular_z > 255)
+    angular_z = 255; 
+  else if (angular_z < 0)
+    angular_z = 0;
+  
+  linear_x = map (linear_x, 0, 255, linear_min, linear_max);
+  angular_z = mapToDouble(angular_z, 0, 255, angular_min, angular_max);
 }
 
-void left(int front, int back) {
-  LeftMF.write(90 + OFFSET - front);
-  LeftMB.write(90 + OFFSET - back);
+void motor_write(int left, int right) {
+  LeftM.write(90 + OFFSET - left);
+  RightM.write(90 + OFFSET - right);
 }
-void right(int front, int back) {
-  RightMF.write(90 + OFFSET + front);
-  RightMB.write(90 + OFFSET + back);
-}
-void drive() {
-  if (az == 90) {
-    if (ly == 90) {
-      right(0, 0); left(0, 0);
-    } else if (ly > 90) {
-      right(FR, BR); left(FL, BL);
-      //Serial.println("FORWARD");
+
+
+/*
+ * moves the robot - turning is taken into account.
+ */
+
+void drive(double angular_speed, int linear_speed) {
+   if (angular_speed < angular_stop) {
+        LeftM.write(linear_speed - round((sin(angular_speed) * WIDTH / 2)));
+        RightM.write(linear_speed + round((sin(angular_speed) * WIDTH / 2)));
     } else {
-      right(-FR, -BR); left(-BL, -BL);
-      //Serial.println("BACK");
+        LeftM.write(linear_speed + round((sin(angular_speed) * WIDTH / 2)));
+        RightM.write(linear_speed - round(sin(angular_speed) * WIDTH / 2));
     }
-  } else if (az < 90) {
-    right(FR, BR); left(-FL, -BL);
-    //Serial.println("RIGHT");
-  } else {
-    right(-FR, -BR); left(FL, BL);
-    //Serial.println("LEFT");
-  }
+}
+
+
+double mapToDouble(int val, int in_min, int in_max, double out_min, double out_max) {
+    return (val - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
