e6c44e33342a4042df2f35cf01ea9e1530f796a2
lkhume <luokehan@gmail.com>
2016-11-05 08:59:30 -0700

    Final decision (#38)
    * Finished FinalDecision node, created test
    
    - finished FinalDecision node
    - implemented tests for FinalDecision node
    
    * added more test cases
    
    -added more test cases to test arbitrator
    
    * added calls and comments to function arbitrator
    
    -added calls to function arbitrator everytime one of the messages changes, and added comments describing what the function is for.
    
    * modified comments
    
    moved comments for all functions to the header file FinalDecision.h above each corresponding function; added comment for turning comment.
    
    * Small Formatting and Spelling Fix

--- src/decision/include/FinalDecision.h
+++ src/decision/include/FinalDecision.h
@@ -16,17 +16,46 @@
 
 class FinalDecision {
 public:
+    // The constructor
     FinalDecision(int argc, char **argv, std::string node_name);
+
+    /**
+     * Arbitrates the three messages received from Lidar, Vision, and GPS
+     *
+     * If there is a change in one or more of the messages, the function decides which one has the highest priority and it is published.
+     * The messages are held in the following order of significance:
+     *      -Lidar
+     *      -Vision
+     *      -GPS
+     *
+     * @param recent_lidar  Twist message from Lidar
+     * @param recent_vision Twist message from the camera, telling the robot where lines and boundaries are
+     * @param recent_gps    Twist message from the GPS
+     *
+     * @return              The Twist message with the highest priority.
+     */
+    static geometry_msgs::Twist arbitrator(geometry_msgs::Twist recent_lidar, geometry_msgs::Twist recent_vision, geometry_msgs::Twist recent_gps);
+
 private:
+    // This is called whenever a new message is received
     void gpsCallBack(const geometry_msgs::Twist::ConstPtr& gps_decision);
+    // This is called whenever a new message is received
     void lidarCallBack(const geometry_msgs::Twist::ConstPtr& lidar_decision);
+    // This is called whenever a new message is received
     void visionCallBack(const geometry_msgs::Twist::ConstPtr& vision_decision);
     void publishTwist(geometry_msgs::Twist twist);
+    // This function evaluates whether GPS, Vision, or Lidar is sending a turning command
+    bool turning(geometry_msgs::Twist twist);
 
     ros::Subscriber lidar_subscriber;
     ros::Subscriber vision_subscriber;
     ros::Subscriber gps_subscriber;
     ros::Publisher twist_publisher;
+    geometry_msgs::Twist recent_lidar;
+    geometry_msgs::Twist recent_vision;
+    geometry_msgs::Twist recent_gps;
+
+
 };
 
 #endif //DECISION_FINAL_DECISION_H
