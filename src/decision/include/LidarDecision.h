8432b0c6c70ff20c1a47683eab36d4e295070de1
Jimmy Yeh <b02202028@ntu.edu.tw>
2016-11-11 02:33:22 -0800

    finish up the function for modifying twist message it will turn right if leftside is blocked and if both side appears to be blocked turn left when rightside is blocked

--- src/decision/include/LidarDecision.h
+++ src/decision/include/LidarDecision.h
@@ -5,6 +5,8 @@
  *              and broadcasts a recommended Twist message
  */
 
+
+
 #ifndef DECISION_LIDAR_DECISION_H
 #define DECISION_LIDAR_DECISION_H
 
@@ -17,7 +19,11 @@
 class LidarDecision {
 public:
     LidarDecision(int argc, char **argv, std::string node_name);
-    static bool obstacle_in_range(double min_angle, double max_angle, float obstacle_distance, const sensor_msgs::LaserScan::ConstPtr& raw_scan);
+    static bool obstacle_in_range(double min_angle, double max_angle, float obstacle_distance,
+                                  const sensor_msgs::LaserScan::ConstPtr& raw_scan);
+
+    static geometry_msgs::Twist manage_twist(float obstacle_distance, float forward_speed,  float angular_speed,
+                                                     const sensor_msgs::LaserScan::ConstPtr& raw_scan);
 private:
     void scanCallBack(const sensor_msgs::LaserScan::ConstPtr& raw_scan);
     void publishTwist(geometry_msgs::Twist twist);
@@ -26,3 +32,4 @@ private:
     ros::Publisher twist_publisher;
 };
 #endif //DECISION_LIDAR_DECISION_H
+
--- src/decision/src/LidarDecision.cpp
+++ src/decision/src/LidarDecision.cpp
@@ -6,8 +6,6 @@
  */
 
 #include <LidarDecision.h>
-
-#include <algorithm>
 using namespace std;
 
 // The constructor
@@ -34,36 +32,66 @@ void LidarDecision::scanCallBack(const sensor_msgs::LaserScan::ConstPtr& raw_sca
     // Deal with new messages here
 }
 
-void LidarDecision::publishTwist(geometry_msgs::Twist twist){
-    twist_publisher.publish(twist);
-}
 
 
 
+
+double pi = 3.1415926;
 
 
 bool LidarDecision::obstacle_in_range(double min_angle, double max_angle, float obstacle_distance,
-                                      const sensor_msgs::LaserScan::ConstPtr& raw_scan){
+                                      const sensor_msgs::LaserScan::ConstPtr& raw_scan) {
     int i;
     int init_pos_vec, end_pos_vec;
 
-    init_pos_vec = static_cast<int>((min_angle - raw_scan->angle_min)/raw_scan->angle_increment);
-    end_pos_vec = static_cast<int>((max_angle - raw_scan->angle_min)/ raw_scan->angle_increment);
+    init_pos_vec = static_cast<int>((min_angle - raw_scan->angle_min) / raw_scan->angle_increment);
+    end_pos_vec = static_cast<int>((max_angle - raw_scan->angle_min) / raw_scan->angle_increment);
+
     /*bool exist_obstacle = false;
-    for (i=init_pos_vec;i<=end_pos_vec;i++){
+    for (i = init_pos_vec; i <= end_pos_vec; i++) {
         if (raw_scan->ranges[i] < obstacle_distance) exist_obstacle = true;
     }
     if (exist_obstacle) return true;
     else return false;*/
     return std::any_of(
-            raw_scan->ranges.begin()+init_pos_vec, raw_scan->ranges.begin()+end_pos_vec,
-            [&](auto const& v){
-                return v < obstacle_distance;
-            });
-}
-
-geometry_msgs::Twist LidarDecision::name(const sensor_msgs::LaserScan::ConstPtr& raw_scan){
-
+             raw_scan->ranges.begin()+init_pos_vec, raw_scan->ranges.begin()+end_pos_vec,
+             [&](auto const& v){
+                 return v < obstacle_distance;
+             });
 
 }
 
+//the twist message to be sent out will be modified throught the following functions
+
+
+geometry_msgs::Twist LidarDecision::manage_twist(float obstacle_distance, float forward_speed,  float angular_speed,
+                                                 const sensor_msgs::LaserScan::ConstPtr& raw_scan){
+    geometry_msgs::Twist vel_msg;
+    bool left_block = LidarDecision::obstacle_in_range(0.0, pi/2, obstacle_distance, raw_scan);
+    bool right_block = LidarDecision::obstacle_in_range(pi/2, pi, obstacle_distance, raw_scan);
+    vel_msg.angular.x = forward_speed;
+    vel_msg.angular.y = 0;
+    vel_msg.linear.x = 0;
+    vel_msg.linear.y = 0;
+    vel_msg.linear.z = 0;
+    if (!left_block and !right_block) vel_msg.angular.z = 0;
+    if (left_block) vel_msg.angular.z = - angular_speed;
+    if (right_block) vel_msg.angular.z = angular_speed;
+    if (right_block and left_block) {
+        vel_msg.linear.x = 0;
+        vel_msg.angular.z = angular_speed;
+    }
+    return vel_msg;
+
+}
+
+
+void LidarDecision::publishTwist(geometry_msgs::Twist twist){
+    twist_publisher.publish(twist);
+}
+
+
+// double range_immediate_front; //the angle range that coresponds to right infront of the robot
+// double a = range_immediate_front;
+//  double safe_distance;
+//bool front_block = LidarDecision::obstacle_in_range(pi/2-a, pi/2+a, safe_distance, raw_scan);
