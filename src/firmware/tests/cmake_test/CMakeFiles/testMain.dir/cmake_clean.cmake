beade3b23320691fe1b863d0f7c531acb26ec1b9
James Asefa <jamesasefa@gmail.com>
2016-11-12 11:45:41 -0800

    RCTest update

---
+++ src/firmware/tests/cmake_test/CMakeFiles/testMain.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testMain.dir/testMain.cpp.o"
+  "testMain.pdb"
+  "testMain"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testMain.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
