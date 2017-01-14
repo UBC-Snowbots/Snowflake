87da7d5d06cfb0d3f4001e6bfdb0202d6cec2697
Gareth Ellis <garethellis0@users.noreply.github.com>
2017-01-14 12:19:49 -0800

    Restored Challenge Files (#60)

---
+++ src/challenge/src/solution.cpp
@@ -0,0 +1,17 @@
+#include <ros/ros.h>
+
+int main(int argc, char **argv){
+    ros::init(argc, argv, "solution");
+    ros::NodeHandle nh;
+
+    ros::Rate loop_rate=10;
+
+    while(ros::ok()){
+
+
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+
+    return 0;
+}
