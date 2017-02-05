959264477ef15a858cb8bd9ce3681a403307ee41
Gareth Ellis <gareth.ellis0@gmail.com>
2017-02-05 13:34:01 -0800

    Moved template function to header file

--- src/sb_utils/include/utils.h
+++ src/sb_utils/include/utils.h
@@ -17,14 +17,24 @@
  * Get a param
  *
  * Gets a param with a given name and sets a given variable to it, or to a
- * default value if the param could not be retrieved
- * @param nh the nodehandle on which to look for the param
+ * default value if the param could not be retrieved.
+ *
+ * Because this is a template function, it's definition must be in the header
+ *
+ * @param nh the nodehandle on which to look for the param,
+ *        should be the private nodehandle
  * @param param_name the name of the param to get
  * @param param_val the variable to set with the retrieved param value
  * @param default_val the value to set param_val to if no param was found
  */
 template <typename T>
 void SB_getParam(ros::NodeHandle& nh, const std::string& param_name,
-                 T& param_val, const T& default_val);
+                 T& param_val, const T& default_val){
+    if (!nh.param(param_name, param_val, default_val)){
+        ROS_WARN_STREAM(nh.getNamespace() << ": " << param_name
+                                          << " not set, defaulting to " << param_val
+        );
+    }
+}
 
 #endif //UTILS_UTILS_H
--- src/sb_utils/src/utils.cpp
+++ src/sb_utils/src/utils.cpp
@@ -10,12 +10,3 @@
 
 #include <utils.h>
 
-template <typename T>
-void SB_getParam(ros::NodeHandle& nh, const std::string& param_name,
-                 T& param_val, const T& default_val){
-    if (!nh.param(param_name, param_val, default_val)){
-        // Casting to a double here is not great, but seems the
-        // only option available with ros's warning system
-        ROS_WARN("%s param not set, defaulting to %f", param_name, (double)param_val);
-    }
-}
