2d5ce9740fe1cdac76000a628d3f5f134f5660a5
Gareth Ellis <gareth.ellis0@gmail.com>
2017-02-12 00:17:00 -0800

    Updated the Setup Script
    - Simplified how we write the new lines to `bashrc` and `zshrc` files,
      as well as making it easier to add more file/lines to add
    - Changed the install location of clion to a more sensible one
    - Updated the clion version
    -

--- get_started.sh
+++ get_started.sh
@@ -6,15 +6,59 @@ echo "Starting first time installation and setup, please wait"
 echo "these downloads can take a while if you're on slow internet."
 echo "================================================================"
 
+# The current directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Shell config files to add aliases to
-SHELL_CONFIG_FILES=("$HOME/.bashrc" "$HOME/.zshrc")
 
-###############################
-# Install ROS
-###############################
+echo "================================================================"
+echo "Setting up your shell config files"
+echo "================================================================"
+# Shell config files that various shells source when they run.
+# This is where we want to add aliases, source ROS environment
+# variables, etc.
+SHELL_CONFIG_FILES=(
+    "$HOME/.bashrc"\
+    "$HOME/.zshrc"
+)
 
+# All lines listed here will be added to the shell config files
+# listed above, if they are not present already
+declare -a new_shell_config_lines=(
+    # Source the ROS Environment Variables Automatically
+    "source /opt/ros/kinetic/setup.sh"\
+    # Make sure that all shells know where to find our custom gazebo models,
+    # plugins, and resources. Make sure to preserve the path that already exists as well
+    "export GAZEBO_MODEL_PATH=$DIR/src/sb_gazebo/models:${GAZEBO_MODEL_PATH}"\
+    "export GAZEBO_PLUGIN_PATH=/home/path/to/IGVC-2017/src/sb_gazebo/lib:${GAZEBO_PLUGIN_PATH}"\
+    "export GAZEBO_RESOURCE_PATH=/home/path/to/IGVC-2017/src/sb_gazebo/models:${GAZEBO_RESOURCE_PATH}"\
+    # Aliases to make development easier
+    "alias clion=\"clion & disown && exit\""\
+    "alias rviz=\"rviz & disown && exit\""\
+    "rqt=\"rqt & disown && exit\""\
+)
+
+# Add all of our new shell config options to all the shell
+# config files, but only if they don't already have them
+for file_name in "${SHELL_CONFIG_FILES[@]}"; 
+do
+    echo "Setting up $file_name"
+    for line in "${new_shell_config_lines[@]}"; 
+    do
+        if ! grep -Fq "$line" $file_name 
+        then
+            echo "$line" >> $file_name 
+        fi
+    done
+done
+
+
+###############
+# Install ROS #
+###############
+
+echo "================================================================"
+echo "Installing ROS Kinetic"
+echo "================================================================"
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 0xB01FA116
 sudo apt-get update
@@ -24,67 +68,39 @@ sudo apt-get install ros-kinetic-desktop-full
 sudo rosdep init
 rosdep update
 
-# Source ROS Environment Variables Automatically
-for file_name in $SHELL_CONFIG_FILES; do
-    if ! grep -Fxq "source /opt/ros/kinetic/setup.bash" $file_name
-    then
-        # Source ROS Environment Variables Automatically
-        echo "source /opt/ros/kinetic/setup.bash" >> $file_name
-    fi
-    if ! grep -Fxq "export GAZEBO_MODEL_PATH=$DIR/src/elsa_gazebo/models:${GAZEBO_MODEL_PATH}" $file_name
-    then
-        # Source ROS Environment Variables Automatically
-        echo "export GAZEBO_MODEL_PATH=$DIR/src/elsa_gazebo/models:${GAZEBO_MODEL_PATH}" >> $file_name
-    fi
-done
-
 # rosinstall allows downloading of source trees for ROS packages 
 # with a single command
 sudo apt-get install python-rosinstall
 
 
-###############################
-# Install CLion
-###############################
-# install by executing `./install.sh`.
-#!/usr/bin/env bash
+#################
+# Install CLion #
+#################
 
+echo "================================================================"
+echo "Installing CLion"
+echo "================================================================"
 # Install dependencies
 sudo apt-get install -y openjdk-8-jdk
 
 # Fetch and extract CLion
 echo "Fetching and extracting CLion"
-wget https://download.jetbrains.com/cpp/CLion-2016.1.3.tar.gz
-tar xzf CLion*.tar.gz -C $DIR
+wget https://download.jetbrains.com/cpp/CLion-2016.3.2.tar.gz
+sudo tar xzf CLion*.tar.gz -C /usr/share
 rm CLion*.tar.gz
 
 # Run CLion Setup
-cd $DIR/clion*
+cd /usr/share/clion*
 ./bin/clion.sh
 
 # Make CLion globally accessible
 echo "Linking CLion"
-sudo ln -s $DIR/clion*/bin/clion.sh /usr/local/bin/clion
+sudo ln -s -f /usr/share/clion*/bin/clion.sh /usr/local/bin/clion
 
-# Change several commands you have to run from the terminal
-# so that they auto-close said terminal
-aliases=("clion=\"clion & disown && exit\""\
-         "rviz=\"rviz & disown && exit\""\
-         "rqt=\"rqt & disown && exit\"")
-# Check to make sure the alias doesn't already exist
-for file_name in $SHELL_CONFIG_FILES; do
-    for ((i = 0; i < ${#aliases[@]}; i++))
-    do
-       if ! grep -F ${aliases[$i]} $file_name
-       then
-          echo "alias ${aliases[$i]}" >> $file_name 
-       fi
-    done
-done
 
-###############################
-# Install Other Dependencies
-###############################
+##############################
+# Install Other Dependencies #
+##############################
 cd $DIR
 ./install_dependencies.sh
 
