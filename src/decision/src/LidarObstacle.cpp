e6204f5ae8f2d6a16e517639790308e6f2ab62e5
raadkhan <raadkhan023@gmail.com>
2017-03-07 20:45:38 -0800

    Implemented a better dangerScore function (#112)
    * initial commit for lidar decition
    
    a function to tell if there is obstacle in certain angle ranges
    
    * finish up the function for modifying twist message
    it will turn right if leftside is blocked and if both side appears to be blocked
    turn left when rightside is blocked
    
    * wrote a second function to replace the first one
    
    can think of this as a three by three grid
    first determine if the obstacle is near range, mid range or far
    than determine whether to turn left or right
    
    haven't tested yet
    after testing need to consider actual sizes (robot, field) to set the distance values.
    
    * test file for version 2 function passed
    
    passed
    
    * initial commit for lidar decition
    
    a function to tell if there is obstacle in certain angle ranges
    
    * finish up the function for modifying twist message
    it will turn right if leftside is blocked and if both side appears to be blocked
    turn left when rightside is blocked
    
    * wrote a second function to replace the first one
    
    can think of this as a three by three grid
    first determine if the obstacle is near range, mid range or far
    than determine whether to turn left or right
    
    haven't tested yet
    after testing need to consider actual sizes (robot, field) to set the distance values.
    
    * test file for version 2 function passed
    
    passed
    
    * modified the file according to comments
    
    finished everything except for the testing parts.
    
    * Failed the test, will try different concept next time
    
    currently using a grid system of near, mid far and assigning rigid values
    would consider another way so that the twist message can turn quicker or slower according to how much space the obstacle is blocking (currently the twist message is indifferent to this)
    
    * Final version
    
    Using the same logic but improved by turning the long if statements in to two functions translating bool values in near, mid, far to corresponding angular and linear velocities
    test succeeded.
    
    * Minor Fixes
    
    - Spelling Fixes in LidarDecision.cpp and lidar_decision.h
    - Removed angular speed function with a conditional operator
    
    * Revamped LidarDecision - Working Commit
    
    - Now uses a system whereby obstacles are detected, the worst on
      classified, and the twist message decided upon based on the worst
      obstacle found
    - This is a working commit, tests still need to be finished for
      LidarDecision, and there are many possible improvements, chiefly
    amoung them a revamp of how the twist messages are generated to make
    use of some sort of non-binary, perhaps exponential model for generating
    angular and linear velocities
    
    * Working commit
    
    * Added more tests
    
    * Removed clion files and added the folder to gitignore
    
    * Fixed ROS_WARN macro
    
    * Fixed some bugs and added rostest tests
    
    * Re-Added gps-manager-test to CMakeLists
    
    (because I screwed up a merge commit :p)
    
    * Probably shouldn't have deleted that.....
    
    * Spelling fix
    
    * Cleaned up and added rostests
    
    - Moved empty lidar scan creation to setup function
    - Created another test for no obstacles
    
    * Added more tests
    
    * Fixed coordinate systems intests and lidar_decision
    
    * Added tests and fixed things
    
    * That should have been 0, not 10....
    
    * Fixed rostest's, because I'm a moron
    
    * Implemented a better dangerScore function
    
    * Implemented a better dangerScore function
    
    * Made some minor fixes
    
    * More minor fixes

--- src/decision/src/LidarObstacle.cpp
+++ src/decision/src/LidarObstacle.cpp
@@ -3,7 +3,7 @@
 /*
  * Created By: Gareth Ellis
  * Created On: January 22, 2017
- * Description: This class represents a Obstacle, as detected by the lidar
+ * Description: This class represents an Obstacle, as detected by the lidar
  */
 
 LidarObstacle::LidarObstacle(){};
@@ -17,17 +17,17 @@ LidarObstacle::LidarObstacle(std::vector<Reading> readings) {
 }
 
 distance_t LidarObstacle::getAvgDistance() {
-    float total_distance = std::accumulate(readings.begin(), readings.end(), 0,
-                                           [] (float accumulator, auto reading){
-                                                return accumulator + reading.range;
-                                            });
+    float total_distance = std::accumulate(readings.begin(), readings.end(), 0.0,
+                                               [] (int accumulator, auto reading){
+                                                       return accumulator + reading.range;
+                                               });
     return total_distance / readings.size();
 }
 
 angle_t LidarObstacle::getAvgAngle() {
     float total_angle = std::accumulate(readings.begin(), readings.end(), 0.0,
-                                        [] (float accumulator, auto reading){
-                                                return accumulator + reading.angle;
+                                            [] (float accumulator, auto reading){
+                                                    return accumulator + reading.angle;
                                             });
     return total_angle / readings.size();
 }
@@ -42,13 +42,36 @@ angle_t LidarObstacle::getMaxAngle() {
     return readings[readings.size()-1].angle;
 }
 
-float LidarObstacle::dangerScore() {
-    return sin(getAvgAngle()) * (1 / getAvgDistance());
-}
-
 const std::vector<Reading>& LidarObstacle::getAllLaserReadings() {
     return readings;
 };
+
+distance_t LidarObstacle::getMinDistance() {
+    std::vector<Reading> readings = getAllLaserReadings();
+    Reading reading_with_min_distance = *std::min_element(readings.begin(), readings.end(),
+                                                          [&] (auto const& reading1, auto const& reading2){
+                                                                   return reading1.range < reading2.range;
+                                                          });
+    return reading_with_min_distance.range;
+}
+
+distance_t LidarObstacle::getMaxDistance() {
+    std::vector<Reading> readings = getAllLaserReadings();
+    Reading reading_with_max_distance = *std::max_element(readings.begin(), readings.end(),
+                                                          [&] (auto const& reading1, auto const& reading2){
+                                                                   return reading1.range > reading2.range;
+                                                          });
+    return reading_with_max_distance.range;
+}
+
+float LidarObstacle::dangerScore() {
+    // angle score increases as an obstacle's angle relative to the robot increases
+    float angle_score = cos(getAvgAngle());
+    // distance score increases as an obstacle's distance relative to the robot increases
+    float distance_score = (1 / getMinDistance());
+    // danger score is sum of angle score and distance score
+    return (angle_score + distance_score);
+}
 
 void LidarObstacle::mergeInReadings(std::vector<Reading> &new_readings) {
     this->readings.insert(readings.end(), new_readings.begin(), new_readings.end());
