392ddb1b28a608955eec470becb98365871663d6
ValRat <leri.ratu@gmail.com>
2017-05-06 19:55:54 -0700

    Reoriented the Gazebo Zed's pointcloud (#175)
    * Added a transform to gazebo's Zed output
    
    * Updated package.xml accordingly
    
    * Updated launch files
    
    * refactoring tf transforms into a templated function

--- src/sb_utils/include/sb_utils.h
+++ src/sb_utils/include/sb_utils.h
@@ -12,6 +12,9 @@
 #define UTILS_UTILS_H
 
 #include <ros/ros.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/buffer.h>
 
 /**
  * Get a param with a default value
@@ -64,6 +67,23 @@ bool SB_getParam(ros::NodeHandle& nh, const std::string& param_name, T& param_va
         return false;
     }
     return true;
+}
+
+/**
+ * Transforms a given message from it's current frame to another frame
+ *
+ * @param input the message to be transformed
+ * @param output the transformed message will be placed in this
+ * @param output_frame the frame to transform the input to
+ */
+template <typename T>
+void SB_doTransform(const T& input, T& output, std::string output_frame){
+    tf2_ros::Buffer tfBuffer;
+    tf2_ros::TransformListener tfListener(tfBuffer);
+    geometry_msgs::TransformStamped transformStamped = tfBuffer.lookupTransform(
+            input.header.frame_id, output_frame, ros::Time(0), ros::Duration(1.0)
+    );
+    tf2::doTransform(input, output, transformStamped);
 }
 
 #endif //UTILS_UTILS_H
