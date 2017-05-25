39cfde33830f1b84646aa3aeeec8cab4405e8f8d
Gareth Ellis <gareth.ellis0@gmail.com>
2017-05-25 15:25:03 -0700

    moved stuff into try-catch block so it\'s in scope

--- src/decision/src/GpsDecision2.cpp
+++ src/decision/src/GpsDecision2.cpp
@@ -53,26 +53,25 @@ void GpsDecision2::waypointCallback(const geometry_msgs::PointStamped::ConstPtr 
     try {
         geometry_msgs::TransformStamped tfStamped = tfBuffer.lookupTransform(
                 global_frame, base_frame, ros::Time(0), ros::Duration(1.0));
+        // Get our current heading and location from the global_frame <-> base_frame tf
+        geometry_msgs::Point current_location;
+        current_location.x = tfStamped.transform.translation.x;
+        current_location.y = tfStamped.transform.translation.y;
+        double current_heading = tf::getYaw(tfStamped.transform.rotation);
+        // Get the non-stamped waypoint, because we don't care about the
+        // stamped info at this point
+        geometry_msgs::Point non_stamped_waypoint = waypoint->point;
+        // Create a new twist message and publish it
+        geometry_msgs::Twist twist = mover.createTwistMessage(
+                current_location, current_heading, non_stamped_waypoint);
+        twist_publisher.publish(twist);
     } catch (tf2::LookupException e) {
         // If we can't lookup the tf, then warn the user and tell robot to stop
-        ROS_WARN_STREAM("Error, could not lookup tf between " <<
+        ROS_WARN_STREAM("Could not lookup tf between " <<
                             global_frame << " and " << base_frame);
         // TODO: Confirm that this is initialized to 0 (or find a nice way to init it to 0)
         geometry_msgs::Twist allZeroTwist;
         twist_publisher.publish(allZeroTwist);
     }
-    // Get our current heading and location from the global_frame <-> base_frame tf
-    geometry_msgs::Point current_location;
-    current_location.x = tfStamped.transform.translation.x;
-    current_location.y = tfStamped.transform.translation.y;
-    double current_heading = tf::getYaw(tfStamped.transform.rotation);
-
-    // Get the non-stamped waypoint, because we don't care about the
-    // stamped info at this point
-    geometry_msgs::Point non_stamped_waypoint = waypoint->point;
-    // Create a new twist message and publish it
-    geometry_msgs::Twist twist = mover.createTwistMessage(
-            current_location, current_heading, non_stamped_waypoint);
-    twist_publisher.publish(twist);
 }
 
