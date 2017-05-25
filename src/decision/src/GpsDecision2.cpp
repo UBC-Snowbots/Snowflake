73e5629a6fc4cfeee065a294f42933c5c3f3f797
Gareth Ellis <gareth.ellis0@gmail.com>
2017-05-25 08:09:07 -0700

    added try-catch for tf::LookupException

--- src/decision/src/GpsDecision2.cpp
+++ src/decision/src/GpsDecision2.cpp
@@ -50,8 +50,18 @@ void GpsDecision2::waypointCallback(const geometry_msgs::PointStamped::ConstPtr 
     // Get the current position and heading of the robot
     tf2_ros::Buffer tfBuffer;
     tf2_ros::TransformListener tfListener(tfBuffer);
-    geometry_msgs::TransformStamped tfStamped = tfBuffer.lookupTransform(
-            global_frame, base_frame, ros::Time(0), ros::Duration(1.0));
+    try {
+        geometry_msgs::TransformStamped tfStamped = tfBuffer.lookupTransform(
+                global_frame, base_frame, ros::Time(0), ros::Duration(1.0));
+    } catch (tf2::LookupException e) {
+        // If we can't lookup the tf, then warn the user and tell robot to stop
+        ROS_WARN_STREAM("Error, could not lookup tf between " <<
+                            global_frame << " and " << base_frame);
+        // TODO: Confirm that this is initialized to 0 (or find a nice way to init it to 0)
+        geometry_msgs::Twist allZeroTwist;
+        twist_publisher.publish(allZeroTwist);
+    }
+    // Get our current heading and location from the global_frame <-> base_frame tf
     geometry_msgs::Point current_location;
     current_location.x = tfStamped.transform.translation.x;
     current_location.y = tfStamped.transform.translation.y;
