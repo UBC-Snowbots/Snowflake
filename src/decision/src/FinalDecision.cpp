3db93ea66a482258ad501d7ff4e996b3c418432d
valrat <leri.ratu@gmail.com>
2017-08-14 22:17:45 -0700

    Initial pull of final decision files

--- src/decision/src/FinalDecision.cpp
+++ src/decision/src/FinalDecision.cpp
@@ -16,9 +16,9 @@ FinalDecision::FinalDecision(int argc, char **argv, std::string node_name) {
     ros::NodeHandle public_nh("~");
 
     // Setup Subscriber(s)
-    std::string lidar_decision_topic_name = "/lidar_decision/command";
-    std::string vision_decision_topic_name = "/vision_decision/command";
-    std::string gps_decision_topic_name = "/gps_decision/command";
+    std::string lidar_decision_topic_name = "/lidar_decision/twist";
+    std::string vision_decision_topic_name = "/vision_decision/twist";
+    std::string gps_decision_topic_name = "/gps_decision/twist";
 
     int refresh_rate = 10;
     lidar_subscriber = public_nh.subscribe(lidar_decision_topic_name, refresh_rate, &FinalDecision::lidarCallBack, this);
@@ -26,7 +26,7 @@ FinalDecision::FinalDecision(int argc, char **argv, std::string node_name) {
     gps_subscriber = public_nh.subscribe(gps_decision_topic_name, refresh_rate, &FinalDecision::gpsCallBack, this);
 
     // Setup Publisher(s)
-    std::string twist_topic = public_nh.resolveName("command");
+    std::string twist_topic = public_nh.resolveName("twist");
     uint32_t queue_size = 10;
     twist_publisher = nh.advertise<geometry_msgs::Twist>(twist_topic, queue_size);
 
@@ -53,18 +53,36 @@ void FinalDecision::gpsCallBack(const geometry_msgs::Twist::ConstPtr& gps_decisi
     arbitrator(recent_lidar,recent_vision,recent_gps);
 }
 
+void FinalDecision::imuCallback(const sensor_msgs::Imu::ConstPtr& imu){
+    last_imu_callback = imu->header.stamp;
+}
+
 void FinalDecision::publishTwist(geometry_msgs::Twist twist){
     twist_publisher.publish(twist);
 }
 
 
 geometry_msgs::Twist FinalDecision::arbitrator(geometry_msgs::Twist recent_lidar, geometry_msgs::Twist recent_vision, geometry_msgs::Twist recent_gps){
+    // TODO: We should probably wait until we have messages from all 3 decision nodes before we make a decision here
+    // TODO: Should we? Don't we want to be able to operate with just some of the nodes?
     if(recent_lidar.angular.z != 0)
-        return recent_lidar;
-    else if(recent_vision.angular.z != 0)
-        return recent_vision;
-    else
-        return recent_gps;
+        publishTwist(recent_lidar);
+    else if(fabs(recent_vision.angular.z) != 0)
+        publishTwist(recent_vision);
+    else {
+        /*
+        if (ros::Time::now() - last_imu_callback > ros::Duration(0.5)) {
+            geometry_msgs::Twist stop; // Technically can assume this will give it 0s, but explicitely written will help;
+            stop.angular.x = 0; stop.angular.y = 0; stop.angular.z = 0;
+            stop.linear.x = 0; stop.linear.y = 0; stop.linear.z = 0;
+            publishTwist(stop);
+        } else {
+            */
+            publishTwist(recent_gps);
+            /*
+        }
+        */
+    }
 }
 
 bool FinalDecision::turning(geometry_msgs::Twist twist){
