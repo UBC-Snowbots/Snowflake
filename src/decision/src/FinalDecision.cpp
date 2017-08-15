1f8e98bf5433543d675b2f56c0dfd18091e2a63e
valrat <leri.ratu@gmail.com>
2017-08-14 23:23:27 -0700

    Cleaned up FinalDecision class and tests

--- src/decision/src/FinalDecision.cpp
+++ src/decision/src/FinalDecision.cpp
@@ -1,13 +1,11 @@
 /*
- * Created By: YOUR NAME HERE
+ * Created By: Kevin Luo
+ * Modified by: Valerian Ratu
  * Created On: September 22, 2016
- * Description: The Decision Node for GPS, takes in a point relative to
- *              the robots location and heading and broadcasts a
- *              recommended twist message
+ * Description: The Final Decision node, arbitrates between the decisions of other nodes.
  */
 
 #include <FinalDecision.h>
-
 
 FinalDecision::FinalDecision(int argc, char **argv, std::string node_name) {
     // Setup NodeHandles
@@ -20,71 +18,43 @@ FinalDecision::FinalDecision(int argc, char **argv, std::string node_name) {
     std::string vision_decision_topic_name = "/vision_decision/twist";
     std::string gps_decision_topic_name = "/gps_decision/twist";
 
-    int refresh_rate = 10;
-    lidar_subscriber = public_nh.subscribe(lidar_decision_topic_name, refresh_rate, &FinalDecision::lidarCallBack, this);
-    vision_subscriber = public_nh.subscribe(vision_decision_topic_name, refresh_rate, &FinalDecision::visionCallBack, this);
-    gps_subscriber = public_nh.subscribe(gps_decision_topic_name, refresh_rate, &FinalDecision::gpsCallBack, this);
+    uint32_t queue_size = 1;
+    lidar_subscriber = public_nh.subscribe(lidar_decision_topic_name, queue_size, &FinalDecision::lidarCallBack, this);
+    vision_subscriber = public_nh.subscribe(vision_decision_topic_name, queue_size, &FinalDecision::visionCallBack, this);
+    gps_subscriber = public_nh.subscribe(gps_decision_topic_name, queue_size, &FinalDecision::gpsCallBack, this);
 
     // Setup Publisher(s)
     std::string twist_topic = public_nh.resolveName("twist");
-    uint32_t queue_size = 10;
     twist_publisher = nh.advertise<geometry_msgs::Twist>(twist_topic, queue_size);
-
 }
 
 
 void FinalDecision::lidarCallBack(const geometry_msgs::Twist::ConstPtr& lidar_decision) {
-    // Deal with new messages here
     recent_lidar = *lidar_decision;
-    arbitrator(recent_lidar,recent_vision,recent_gps);
+    publishTwist(arbitrator(recent_lidar,recent_vision,recent_gps));
 }
-
 
 void FinalDecision::visionCallBack(const geometry_msgs::Twist::ConstPtr& vision_decision) {
-    // Deal with new messages here
     recent_vision = *vision_decision;
-    arbitrator(recent_lidar,recent_vision,recent_gps);
+    publishTwist(arbitrator(recent_lidar,recent_vision,recent_gps));
 }
-
 
 void FinalDecision::gpsCallBack(const geometry_msgs::Twist::ConstPtr& gps_decision) {
-    // Deal with new messages here
     recent_gps = *gps_decision;
-    arbitrator(recent_lidar,recent_vision,recent_gps);
-}
-
-void FinalDecision::imuCallback(const sensor_msgs::Imu::ConstPtr& imu){
-    last_imu_callback = imu->header.stamp;
+    publishTwist(arbitrator(recent_lidar,recent_vision,recent_gps));
 }
 
 void FinalDecision::publishTwist(geometry_msgs::Twist twist){
     twist_publisher.publish(twist);
 }
 
-
 geometry_msgs::Twist FinalDecision::arbitrator(geometry_msgs::Twist recent_lidar, geometry_msgs::Twist recent_vision, geometry_msgs::Twist recent_gps){
-    // TODO: We should probably wait until we have messages from all 3 decision nodes before we make a decision here
-    // TODO: Should we? Don't we want to be able to operate with just some of the nodes?
-    if(recent_lidar.angular.z != 0)
-        publishTwist(recent_lidar);
-    else if(fabs(recent_vision.angular.z) != 0)
-        publishTwist(recent_vision);
-    else {
-        /*
-        if (ros::Time::now() - last_imu_callback > ros::Duration(0.5)) {
-            geometry_msgs::Twist stop; // Technically can assume this will give it 0s, but explicitely written will help;
-            stop.angular.x = 0; stop.angular.y = 0; stop.angular.z = 0;
-            stop.linear.x = 0; stop.linear.y = 0; stop.linear.z = 0;
-            publishTwist(stop);
-        } else {
-            */
-            publishTwist(recent_gps);
-            /*
-        }
-        */
+    if(recent_lidar.angular.z != 0) {
+        return recent_lidar;
+    } else if (fabs(recent_vision.angular.z) != 0) {
+        return recent_vision;
+    } else {
+        return recent_gps;
     }
 }
 
-bool FinalDecision::turning(geometry_msgs::Twist twist){
-    return (twist.angular.z != 0);
-}
