ff1c94b4b055358817b19fe4d948294cd132265d
Gareth Ellis <gareth.ellis0@gmail.com>
2017-08-15 18:35:33 -0700

    added updated gps_manager and gps_decision, and cleaned up decision CMakeLists.txt

--- src/decision/src/GpsDecision.cpp
+++ src/decision/src/GpsDecision.cpp
@@ -1,9 +1,9 @@
 /*
- * Created By: Gareth Ellis and Chris Chen
- * Created On: September 22, 2016
+ * Created By: Gareth Ellis
+ * Created On: May 18, 2017
  * Description: The Decision Node for GPS, takes in a point relative to
  *              the robots location and heading and broadcasts a
- *              recommended twist message
+ *              recommended twist message. Revised to use TF's
  */
 
 #include <GpsDecision.h>
@@ -17,12 +17,6 @@ GpsDecision::GpsDecision(int argc, char **argv, std::string node_name) {
 
     // Setup Subscriber(s)
     uint32_t refresh_rate = 10;
-    std::string current_location_topic = "/gps_manager/current_location";
-    current_location_subscriber = private_nh.subscribe(current_location_topic, refresh_rate,
-                                                    &GpsDecision::currentLocationCallback, this);
-    std::string imu_topic = "/gps_manager/current_heading";
-    imu_subscriber = private_nh.subscribe(imu_topic, refresh_rate,
-                                          &GpsDecision::imuCallback, this);
     std::string waypoint_topic = "/gps_manager/current_waypoint";
     waypoint_subscriber = private_nh.subscribe(waypoint_topic, refresh_rate,
                                                &GpsDecision::waypointCallback, this);
@@ -36,29 +30,48 @@ GpsDecision::GpsDecision(int argc, char **argv, std::string node_name) {
     // based on our distance and heading to the destination GPS waypoint
     double linear_heading_factor, linear_distance_factor,
             angular_heading_factor, angular_distance_factor;
-    double linear_cap, angular_cap;
     SB_getParam(private_nh, "linear_heading_factor", linear_heading_factor, 1.0);
     SB_getParam(private_nh, "linear_distance_factor", linear_distance_factor, 1.0);
     SB_getParam(private_nh, "angular_heading_factor", angular_heading_factor, 1.0);
     SB_getParam(private_nh, "angular_distance_factor", angular_distance_factor, 1.0);
     mover.setFactors(linear_distance_factor, linear_heading_factor,
                     angular_distance_factor, angular_heading_factor);
+
+    double linear_cap, angular_cap;
     SB_getParam(private_nh, "max_linear_speed", linear_cap, 1.0);
     SB_getParam(private_nh, "max_angular_speed", angular_cap, 1.0);
     mover.setMaxSpeeds(linear_cap, angular_cap);
+
+    SB_getParam(private_nh, "base_frame", base_frame, (std::string)"base_link");
+    SB_getParam(private_nh, "global_frame", global_frame, (std::string)"odom_combined");
 }
 
-void GpsDecision::currentLocationCallback(const geometry_msgs::Point::ConstPtr &current_location) {
-    this->current_location = *current_location;
-}
-
-void GpsDecision::imuCallback(const std_msgs::Float32::ConstPtr &heading) {
-    this->current_heading = heading->data;
-}
-
-void GpsDecision::waypointCallback(const geometry_msgs::Point::ConstPtr &waypoint) {
-    // Create a new twist message and publish it
-    geometry_msgs::Twist twist = mover.createTwistMessage(current_location, current_heading, *waypoint);
-    twist_publisher.publish(twist);
+void GpsDecision::waypointCallback(const geometry_msgs::PointStamped::ConstPtr &waypoint) {
+    // Get the current position and heading of the robot
+    tf2_ros::Buffer tfBuffer;
+    tf2_ros::TransformListener tfListener(tfBuffer);
+    try {
+        geometry_msgs::TransformStamped tfStamped = tfBuffer.lookupTransform(
+                global_frame, base_frame, ros::Time(0), ros::Duration(1.0));
+        // Get our current heading and location from the global_frame <-> base_frame tf
+        geometry_msgs::Point current_location;
+        current_location.x = tfStamped.transform.translation.x;
+        current_location.y = tfStamped.transform.translation.y;
+        double current_heading = tf::getYaw(tfStamped.transform.rotation);
+        // Get the non-stamped waypoint, because we don't care about the
+        // stamped info at this point
+        geometry_msgs::Point non_stamped_waypoint = waypoint->point;
+        // Create a new twist message and publish it
+        geometry_msgs::Twist twist = mover.createTwistMessage(
+                current_location, current_heading, non_stamped_waypoint);
+        twist_publisher.publish(twist);
+    } catch (tf2::LookupException e) {
+        // If we can't lookup the tf, then warn the user and tell robot to stop
+        ROS_WARN_STREAM("Could not lookup tf between " <<
+                            global_frame << " and " << base_frame);
+        // TODO: Confirm that this is initialized to 0 (or find a nice way to init it to 0)
+        geometry_msgs::Twist allZeroTwist;
+        twist_publisher.publish(allZeroTwist);
+    }
 }
 
--- src/decision/src/GpsManager.cpp
+++ src/decision/src/GpsManager.cpp
@@ -1,28 +1,10 @@
 /*
  * Created By: Gareth Ellis
- * Created On: October 20, 2016
+ * Created On: May 17, 2017
  * Description: Manages the GPS waypoints we are given
  *              (publishing the next one once we've arrived
  *              at the current one)
  */
-
-/*
- * ~~~~~~~~~~~ CONVENTIONS USED ~~~~~~~~~~~
-              +X
-              ^
-              |
-      -θ  +<----->+ +θ
-          |   |   |
-          V   |   V
-+Y <---------------------> -Y
-              |
-              |
-              |
-              V
-              -X
-
- */
-
 
 #include "GpsManager.h"
 
@@ -33,134 +15,63 @@ GpsManager::GpsManager(int argc, char **argv, std::string node_name){
     ros::NodeHandle private_nh("~");
 
     // Setup Subscribers
-    uint32_t refresh_rate = 1;
-    std::string raw_gps_topic_name = "/gps_driver/navsatfix";
-    raw_gps_subscriber = private_nh.subscribe(raw_gps_topic_name, refresh_rate,
-                                             &GpsManager::rawGpsCallBack, this);
-    std::string imu_topic_name = "/imu";
-    imu_subscriber = private_nh.subscribe(imu_topic_name, refresh_rate,
-                                             &GpsManager::imuCallback, this);
+    uint32_t queue_size = 1;
+    std::string tf_topic_name = "/tf";
+    tf_subscriber = private_nh.subscribe(tf_topic_name, queue_size,
+                                        &GpsManager::tfCallBack, this);
 
     // Setup Publishers
-    uint32_t queue_size = 1;
-    std::string current_location_topic = private_nh.resolveName("current_location");
-    current_location_publisher = nh.advertise<geometry_msgs::Point>(current_location_topic, queue_size);
     std::string current_waypoint_topic = private_nh.resolveName("current_waypoint");
-    current_waypoint_publisher = nh.advertise<geometry_msgs::Point>(current_waypoint_topic, queue_size);
-    std::string current_heading_topic = private_nh.resolveName("current_heading");
-    current_heading_publisher = nh.advertise<std_msgs::Float32>(current_heading_topic, queue_size);
+    current_waypoint_publisher = nh.advertise<geometry_msgs::PointStamped>
+            (current_waypoint_topic, queue_size);
 
     // Get Params
+    SB_getParam(private_nh, "base_frame", base_frame, (std::string)"base_link");
+    SB_getParam(private_nh, "global_frame", global_frame, (std::string)"odom_combined");
     SB_getParam(private_nh, "at_goal_tolerance", at_goal_tolerance, (float)1.0);
-    if (!SB_getParam(private_nh, "waypoints", waypoints_raw)){
+    std::vector<double> waypoints_raw; // The raw list of waypoints retrieved from the param server
+    if (!SB_getParam(private_nh, "waypoints", waypoints_raw)) {
         ROS_ERROR("Waypoints should be a list in the form [lat, lon, lat, lon, ...]");
+        ros::shutdown();
     } else {
-        waypoint_list = parseWaypoints(waypoints_raw);
-        populateWaypointStack();
-    }
-
-    // Initialize misc. variables
-    received_initial_navsatfix = false;
-    origin_heading = -1;
-    most_recent_heading = -1;
-
-    ROS_INFO("Hold on a sec, just waiting on initial compass and gps readings");
-}
-
-void GpsManager::imuCallback(const sensor_msgs::Imu::ConstPtr imu_msg){
-    // Get current heading (yaw) from the imu message
-    float curr_heading = (float)tf::getYaw(imu_msg->orientation);
-    // If this is the first received compass message, make it origin_heading
-    if (origin_heading == -1){
-        origin_heading = curr_heading;
-        ROS_INFO("Received initial compass reading");
-        if (received_initial_navsatfix){
-            ROS_INFO("Received initial compass and gps readings, good to go!");
-        }
-    }
-    // Received heading is now the most recent one
-    most_recent_heading = curr_heading;
-    // Publish the current heading of the robot, relative to the initial heading being 0 degrees
-    publishTranslatedHeading(curr_heading);
-}
-
-void GpsManager::rawGpsCallBack(const sensor_msgs::NavSatFix::ConstPtr nav_sat_fix) {
-    // Set origin_navsatfix to the first navsatfix we receive
-    if (!received_initial_navsatfix){
-        origin_navsatfix = *nav_sat_fix;
-        received_initial_navsatfix = true;
-        ROS_INFO("Received initial NavSatFix");
-        if (origin_heading != -1){
-            ROS_INFO("Received initial compass and gps readings, good to go!");
-        }
-    }
-    // Make sure we've got origin readings before broadcasting waypoints
-    if (origin_heading != -1 && received_initial_navsatfix) {
-        // If we're at the goal, start publishing the next waypoint
-        if (waypoint_stack.size() > 0 && distanceToNextWaypoint(*nav_sat_fix) < at_goal_tolerance) {
-            ROS_INFO("\nHit waypoint \nlat: %f \nlon: %f",
-                    waypoint_stack.top().lat, waypoint_stack.top().lon);
-            // Start going to the next waypoint
-            waypoint_stack.pop();
-        }
-        if (waypoint_stack.size() > 0) {
-            publishWaypoint(waypoint_stack.top());
-        } else {
-            ROS_INFO("Visited all waypoints");
-            // TODO: This will probably need to be handled more gracefully. Right now, the robot will probably get to the final waypoint and freak out
-        }
-        // Convert the nav_sat_fix to a Point with x,y in meters, and relative to the starting location
-        // and rotation
-        publishCurrentLocation(*nav_sat_fix);
+        std::vector<Waypoint>waypoint_list = parseWaypoints(waypoints_raw);
+        populateWaypointStack(waypoint_list);
     }
 }
 
-void GpsManager::publishTranslatedHeading(float heading){
-    // Subtract the initial heading from the current heading
-    std_msgs::Float32 translated_heading = std_msgs::Float32();
-    translated_heading.data = heading - origin_heading;
-    current_heading_publisher.publish(translated_heading);
-}
+void GpsManager::tfCallBack(const tf2_msgs::TFMessageConstPtr tf_message){
+    // If we've visited all the waypoints, no need to do anything here,
+    // In fact, we can celebrate and shutdown
+    if (waypoint_stack.size() <= 0){
+        ROS_INFO_STREAM("Reached all wapoints! *cue digital clapping*");
+        ros::shutdown();
+        return;
+    }
 
-void GpsManager::publishCurrentLocation(sensor_msgs::NavSatFix curr_location){
-    Waypoint tmp_waypoint;
-    tmp_waypoint.lat = curr_location.latitude;
-    tmp_waypoint.lon = curr_location.longitude;
-    geometry_msgs::Point point_msg = convertToRobotsPerspective(
-            tmp_waypoint, origin_navsatfix, origin_heading);
-    current_location_publisher.publish(point_msg);
-}
+    // Publish the current waypoint
+    current_waypoint_publisher.publish(waypoint_stack.top());
 
-void GpsManager::publishWaypoint(Waypoint waypoint){
-    geometry_msgs::Point point_msg = convertToRobotsPerspective(waypoint, origin_navsatfix, origin_heading);
-    current_waypoint_publisher.publish(point_msg);
-}
-
-geometry_msgs::Point GpsManager::convertToRobotsPerspective(Waypoint waypoint,
-                                                            sensor_msgs::NavSatFix origin_navsatfix,
-                                                            double origin_heading){
-    Waypoint origin_waypoint;
-    origin_waypoint.lat = origin_navsatfix.latitude;
-    origin_waypoint.lon = origin_navsatfix.longitude;
-    // Get distance and angle between the waypoint and the origin
-    double origin_to_waypoint_distance = distanceBetweenWaypoints(origin_waypoint, waypoint);
-    double origin_to_waypoint_angle = angleBetweenWaypoints(origin_waypoint, waypoint);
-    // Convert the waypoint to x,y coordinates in the robots frame of reference
-    double gps_x = cos(origin_to_waypoint_angle) * origin_to_waypoint_distance; // x value from gps perspective
-    double gps_y = sin(origin_to_waypoint_angle) * origin_to_waypoint_distance; // y value from gps perspective
-    // Convert gps x,y to robot's frame of reference using a rotation matrix
-    geometry_msgs::Point point_msg;
-    origin_heading *= -1;
-    point_msg.x = cos(origin_heading) * gps_x + sin(origin_heading) * gps_y;
-    point_msg.y = sin(origin_heading) * gps_x - cos(origin_heading) * gps_y;
-    return point_msg;
+    // Check if the tf_message contains the transform we're looking for
+    for (geometry_msgs::TransformStamped tf_stamped : tf_message->transforms){
+        if (tf_stamped.header.frame_id == global_frame && tf_stamped.child_frame_id == base_frame){
+            // We've found the transform we're looking for, so see how close we are to the waypoint
+            double dx = tf_stamped.transform.translation.x - waypoint_stack.top().point.x;
+            double dy = tf_stamped.transform.translation.y - waypoint_stack.top().point.y;
+            double distance = sqrt(pow(dx,2) + pow(dy,2));
+            if (distance < at_goal_tolerance){
+                waypoint_stack.pop();
+                ROS_INFO_STREAM("Hit waypoint " << waypoint_stack.size());
+            }
+        }
+    }
 }
 
 std::vector<Waypoint> GpsManager::parseWaypoints(std::vector<double> waypoints_raw){
     if (waypoints_raw.size() % 2 != 0){
         ROS_ERROR("Given odd length list of waypoints, check that \"waypoints\" parameter "
-                  "has an (numerically) even length. As this is a series of lat/lon coordinates,"
-                  "it should always be even");
+                          "has an (numerically) even length. As this is a series of lat/lon coordinates,"
+                          "it should always be even");
+        ros::shutdown();
         return {};
     }
 
@@ -175,45 +86,23 @@ std::vector<Waypoint> GpsManager::parseWaypoints(std::vector<double> waypoints_r
     return waypoints;
 }
 
-double GpsManager::distanceToNextWaypoint(sensor_msgs::NavSatFix curr_navsatfix){
-    Waypoint temp_waypoint;
-    temp_waypoint.lat = curr_navsatfix.latitude;
-    temp_waypoint.lon = curr_navsatfix.longitude;
-    return distanceBetweenWaypoints(temp_waypoint, waypoint_stack.top());
-}
-
-double GpsManager::distanceBetweenWaypoints(Waypoint waypoint_1, Waypoint waypoint_2){
-    // Convert lat/lon from degrees to radians
-    double lat1 = waypoint_1.lat * (M_PI/180);
-    double lon1 = waypoint_1.lon * (M_PI/180);
-    double lat2 = waypoint_2.lat * (M_PI/180);
-    double lon2 = waypoint_2.lon * (M_PI/180);
-
-    // Find distance using Haversine formula
-    double distance = pow(sin((lat2 - lat1)/2), 2) + cos(lat1)* cos(lat2) * pow(sin((lon2 - lon1)/2),2);
-    distance = 2 * atan2(sqrt(distance), sqrt(1-distance));
-    distance *= 6371000; // Earth Radius in meters
-
-    return distance;
-}
-
-double GpsManager::angleBetweenWaypoints(Waypoint waypoint_1, Waypoint waypoint_2){
-    // Convert lat/lon from degrees to radians
-    double lat1 = waypoint_1.lat * (M_PI/180);
-    double lon1 = waypoint_1.lon * (M_PI/180);
-    double lat2 = waypoint_2.lat * (M_PI/180);
-    double lon2 = waypoint_2.lon * (M_PI/180);
-
-    double delta_lon = lon2 - lon1;
-    double y = sin(delta_lon) * cos(lat2);
-    double x = cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(delta_lon);
-    // Atan2 returns a value between -Pi and Pi, which we want to normalize to a compass bearing
-    double angle = fmod((atan2(y,x) + (2 * M_PI)), 2 * M_PI);
-    return angle;
-}
-
-void GpsManager::populateWaypointStack(){
+void GpsManager::populateWaypointStack(std::vector<Waypoint> waypoint_list){
     for (int i = (int)waypoint_list.size() - 1; i >= 0; i--){
-        waypoint_stack.push(waypoint_list[i]);
+        // Convert the waypoint to UTM
+        double northing, easting;
+        std::string zone_throwaway;
+        gps_common::LLtoUTM(waypoint_list[i].lat, waypoint_list[i].lon, northing, easting, zone_throwaway);
+
+        // Build a point with our UTM coordinates
+        geometry_msgs::PointStamped point_stamped;
+        point_stamped.header.frame_id = base_frame;
+        point_stamped.header.stamp = ros::Time();
+        point_stamped.point.x = northing;
+        point_stamped.point.y = -easting;
+
+        waypoint_stack.push(point_stamped);
     }
+
+    ROS_INFO_STREAM("Ready to rock 'n' roll with " << waypoint_stack.size() << " waypoints!");
 }
+
