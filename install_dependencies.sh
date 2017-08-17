addb54d3ad43665656f56a807e8811a65d95878b
Gareth Ellis <gareth.ellis0@gmail.com>
2017-08-16 17:56:52 -0700

    added gps_umd as a submodule so we can use the utm_odometry_node

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -15,13 +15,17 @@ echo "Installing other ROS dependencies..."
 echo "================================================================"
 
 # Update Rosdeps
-rosdep update
+#rosdep update
 
 # The current directory
 CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Installs all required dependencies to build this repo
+# Install all required dependencies to build this repo
 rosdep install --from-paths src --ignore-src --rosdistro kinetic -y
+
+# We have this extra rosdep call because rosdep can't seem to find nested
+# `package.xml` files, like those in src/gps_umd/gps_common
+rosdep install --from-paths src/gps_umd --rosdistro kinetic -y
 
 echo "================================================================"
 echo "Finished installing other ROS dependencies."
