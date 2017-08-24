b20572b4ce6ad9a4ed5244cfb477d497643e3e36
Gareth Ellis <gareth.ellis0@gmail.com>
2017-08-23 20:27:25 -0700

    causing clang format to fail

--- src/decision/src/GpsDecision.cpp
+++ src/decision/src/GpsDecision.cpp
@@ -11,7 +11,8 @@
 
 GpsDecision::GpsDecision(int argc, char **argv, std::string node_name) {
     // Setup NodeHandles
-    ros::init(argc, argv, node_name);
+    ros::init(argc, 
+                                argv, node_name);
     ros::NodeHandle nh;
     ros::NodeHandle private_nh("~");
 
