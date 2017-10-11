d9a9b178c16d325a5a6c590404ceabd5588f9242
Gareth Ellis <gareth.ellis0@gmail.com>
2017-10-11 04:21:26 -0700

    Reverted some changes from merging core into igvc
    Because we branched igvc to core, then deleted all the igvc specific
    code, the merge passed all the deletions over back to igvc. Obviously
    we don't want this, and hopefully by just making another commit that
    reverts the changes, we can avoid this issue in the future.

---
+++ src/drivers/test/steering-driver-test.cpp
@@ -0,0 +1,14 @@
+/*
+ * Created By: Gareth Ellis
+ * Created On: July 16th, 2016
+ * Description: Tests for MyNode
+ */
+
+#include <SteeringDriver.h>
+#include <gtest/gtest.h>
+
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
