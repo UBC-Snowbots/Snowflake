879dde7403c75518c95f5ac6b990cda0d678f8dd
Gareth Ellis <gareth.ellis0@gmail.com>
2017-10-21 11:46:37 -0700

    fixed formatting

--- src/lane_follow/include/LaneFollow.h
+++ src/lane_follow/include/LaneFollow.h
@@ -10,39 +10,36 @@
 
 #include <LineDetect.h>
 
-#include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/opencv.hpp>
 
-#include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
+#include <ros/ros.h>
 #include <sensor_msgs/Image.h>
 
-#include <sb_utils.h>
 #include <IPM.h>
+#include <sb_utils.h>
 
 #include <cv_bridge/cv_bridge.h>
-#include <sensor_msgs/image_encodings.h>
 #include <image_transport/image_transport.h>
+#include <sensor_msgs/image_encodings.h>
 
 class LaneFollow {
-
-public:
-
+  public:
     /**
      * Constructor
      */
-    LaneFollow(int argc, char **argv, std::string node_name);
+    LaneFollow(int argc, char** argv, std::string node_name);
 
-private:
-
+  private:
     /**
      * Callback for the filtered image
      *
      * @param address of filtered image matrix
      */
-    void subscriberCallBack(const sensor_msgs::ImageConstPtr &msg);
+    void subscriberCallBack(const sensor_msgs::ImageConstPtr& msg);
 
     // Angle of POI of detected lane lines relative to the robot
     int angle_theta;
@@ -52,9 +49,12 @@ private:
      *
      * @params image information and where to apply the IPM
      */
-    void createFilter(float ipm_base_width, float ipm_top_width,
-                      float ipm_base_displacement, float ipm_top_displacement,
-                      float image_height, float image_width);
+    void createFilter(float ipm_base_width,
+                      float ipm_top_width,
+                      float ipm_base_displacement,
+                      float ipm_top_displacement,
+                      float image_height,
+                      float image_width);
 
     // Instantiate LineDetect class which generates the lane lines
     LineDetect ld;
@@ -73,7 +73,8 @@ private:
 
     // IPM
     IPM ipm;
-    float ipm_base_width, ipm_top_width, ipm_base_displacement, ipm_top_displacement;
+    float ipm_base_width, ipm_top_width, ipm_base_displacement,
+    ipm_top_displacement;
 
     // Whether or not we've received the first image
     bool receivedFirstImage;
@@ -98,10 +99,10 @@ private:
      *
      * @param message to be converted
      */
-    cv::Mat rosToMat(const sensor_msgs::Image::ConstPtr &image);
+    cv::Mat rosToMat(const sensor_msgs::Image::ConstPtr& image);
 
-    std::vector <std::vector<cv::Point2d>> transformPoints(std::vector <std::vector<cv::Point2d>> filteredPoints);
-
+    std::vector<std::vector<cv::Point2d>>
+    transformPoints(std::vector<std::vector<cv::Point2d>> filteredPoints);
 };
 
 #endif
