0add01c342a1c15b4b41a4c9534e0c12da04399b
valrat <leri.ratu@gmail.com>
2017-10-28 13:38:27 -0700

    Robyn's suggested changes

--- src/firmware/olaf_control/olaf_control.ino
+++ src/firmware/olaf_control/olaf_control.ino
@@ -1,12 +1,12 @@
 /* Firmware for IARRC 2017
-   Author: Vincent Yuan, 
-   Modified: Nick Wu
-   Modified: James Asefa
-   Modified: Jinhao Lu
-   Modified: Gareth Ellis
-   Modified: Valerian Ratu
-   Date Last Modified: October 22, 2017
-*/
+ * Author: Vincent Yuan, 
+ * Modified: Nick Wu
+ * Modified: James Asefa
+ * Modified: Jinhao Lu
+ * Modified: Gareth Ellis
+ * Modified: Valerian Ratu
+ * Date Last Modified: October 22, 2017
+ */
 
 /*
  * UBC Snowbots - IARRC 2017
@@ -23,6 +23,9 @@
 #include <stdlib.h>
 #include <Servo.h>
 
+// !!!!WARNING!!!! 
+// DEBUGGING might affect performance and alter normal behaviour
+// !!!!WARNING!!!! 
 // Uncomment this to enable debug message logging - messages sent over serial will be echoed back
 //#define DEBUG_MESSAGES
 // Uncomment this to enable PWM debug logging - PWM signals will be echoed
@@ -92,84 +95,84 @@ Servo motor;
 Servo direction_motor;
 
 void setup() {
-  Serial.begin(BAUD_RATE);
-  
-  // Initialiase the Servo and Motor connections
-	motor.attach(MOTOR_PIN, MIN_PWM, MAX_PWM);
-  direction_motor.attach(DIRECTION_PIN, MIN_PWM, MAX_PWM);
- }
+    Serial.begin(BAUD_RATE);
+
+    // Initialiase the Servo and Motor connections
+    motor.attach(MOTOR_PIN, MIN_PWM, MAX_PWM);
+    direction_motor.attach(DIRECTION_PIN, MIN_PWM, MAX_PWM);
+}
 
 void loop() {
-	serial_read(); 
-	convert();
-	drive(linear_x, angular_z);
+    serial_read(); 
+    convert();
+    drive(linear_x, angular_z);
 }
 
 void serial_read(){
-  // BUFFER_HEAD identifies the start of the buffer
-	if ( Serial.available() >= BUFFER_SIZE && Serial.read() == BUFFER_HEAD ) {
-		linear_x = Serial.read();
-		linear_y = Serial.read();
-		linear_z = Serial.read();
-		angular_x = Serial.read();
-		angular_y = Serial.read();
-		angular_z = Serial.read();
-		previousMillis = currentMillis;
-   
-   #ifdef DEBUG_MESSAGES
-    Serial.println("Got message");
-    Serial.print("Linear X:"); Serial.println(linear_x);
-    Serial.print("Linear Y:"); Serial.println(linear_y);
-    Serial.print("Linear Z:"); Serial.println(linear_z);
-    Serial.print("Angular X:"); Serial.println(angular_x);
-    Serial.print("Angular Y:"); Serial.println(angular_y);
-    Serial.print("Angular Z:"); Serial.println(angular_z);
-   #endif
-	} else {
-	  currentMillis = millis();
-    if(currentMillis - previousMillis > SAFETY_TIMEOUT){
-	    linear_x = UNMAPPED_STOP_SPEED;
+    // BUFFER_HEAD identifies the start of the buffer
+    if ( Serial.available() >= BUFFER_SIZE && Serial.read() == BUFFER_HEAD ) {
+        linear_x = Serial.read();
+        linear_y = Serial.read();
+        linear_z = Serial.read();
+        angular_x = Serial.read();
+        angular_y = Serial.read();
+        angular_z = Serial.read();
+        previousMillis = currentMillis;
+
+#ifdef DEBUG_MESSAGES
+        Serial.println("Got message");
+        Serial.print("Linear X:"); Serial.println(linear_x);
+        Serial.print("Linear Y:"); Serial.println(linear_y);
+        Serial.print("Linear Z:"); Serial.println(linear_z);
+        Serial.print("Angular X:"); Serial.println(angular_x);
+        Serial.print("Angular Y:"); Serial.println(angular_y);
+        Serial.print("Angular Z:"); Serial.println(angular_z);
+#endif
+    } else {
+        currentMillis = millis();
+        if(currentMillis - previousMillis > SAFETY_TIMEOUT){
+            linear_x = UNMAPPED_STOP_SPEED;
+        }
     }
-	}
 }
 
 void convert() {
-	// safety check if values are out of range
-	if (linear_x > LINEAR_MAX)
-		linear_x = LINEAR_MAX; 
-	else if (linear_x < LINEAR_MIN)
-		linear_x = LINEAR_MIN;
+    // safety check if values are out of range
+    if (linear_x > LINEAR_MAX)
+        linear_x = LINEAR_MAX; 
+    else if (linear_x < LINEAR_MIN)
+        linear_x = LINEAR_MIN;
 
-	if (angular_z > ANGULAR_MAX)
-		angular_z = ANGULAR_MAX; 
-	else if (angular_z < ANGULAR_MIN)
-		angular_z = ANGULAR_MIN;
+    if (angular_z > ANGULAR_MAX)
+        angular_z = ANGULAR_MAX; 
+    else if (angular_z < ANGULAR_MIN)
+        angular_z = ANGULAR_MIN;
 }
 
 void drive(int linear_speed, int angular_speed){
-  // Default velocity and angle to the midpoint in the PWM range
-	int velocity = (MAX_PWM - MIN_PWM)/2;
-	int angle = (MAX_PWM - MIN_PWM)/2;
+    // Default velocity and angle to the midpoint in the PWM range
+    int velocity = (MAX_PWM - MIN_PWM)/2;
+    int angle = (MAX_PWM - MIN_PWM)/2;
 
-	velocity = map(linear_speed, LINEAR_MIN, LINEAR_MAX, MIN_PWM, MAX_PWM); 
-	angle = map(angular_speed, ANGULAR_MIN, ANGULAR_MAX, MIN_PWM, MAX_PWM);
+    velocity = map(linear_speed, LINEAR_MIN, LINEAR_MAX, MIN_PWM, MAX_PWM); 
+    angle = map(angular_speed, ANGULAR_MIN, ANGULAR_MAX, MIN_PWM, MAX_PWM);
 
-  // Check that we're not outside our safety cutoffs
-  if (velocity > MAX_MOTOR_PWM_CUTOFF)
-    velocity = MAX_MOTOR_PWM_CUTOFF;
-  else if (velocity < MIN_MOTOR_PWM_CUTOFF)
-    velocity = MIN_MOTOR_PWM_CUTOFF;
+    // Check that we're not outside our safety cutoffs
+    if (velocity > MAX_MOTOR_PWM_CUTOFF)
+        velocity = MAX_MOTOR_PWM_CUTOFF;
+    else if (velocity < MIN_MOTOR_PWM_CUTOFF)
+        velocity = MIN_MOTOR_PWM_CUTOFF;
 
-  // Write the commands to the motor and the servo
-	motor.writeMicroseconds(velocity);
-	direction_motor.writeMicroseconds(angle);
+    // Write the commands to the motor and the servo
+    motor.writeMicroseconds(velocity);
+    direction_motor.writeMicroseconds(angle);
 
-  #ifdef DEBUG_PWM
+#ifdef DEBUG_PWM
     Serial.print(velocity);Serial.print(" / ");
     Serial.print(angle);Serial.print(" / ");
     Serial.print(linear_speed);Serial.print(" / ");
     Serial.print(angular_speed);Serial.print(" / ");
     Serial.println();
-  #endif
+#endif
 }
 
