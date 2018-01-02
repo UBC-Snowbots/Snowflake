0be9a1e303a5fcfd5815685353410953efca4bc2
Gareth Ellis <gareth.ellis0@gmail.com>
2018-01-02 12:05:25 -0800

    added a default branch option to the formatting script

--- clang-format/fix_formatting.sh
+++ clang-format/fix_formatting.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 
+# This script can be called with no arguments (`./fix_formatting`), in which 
+# case DEFAULT_BRANCH will be used as the branch to compare against to find 
+# changes to format, or it can be called with a specific branch to compare 
+# against (`./fix_formatting master`)
+
 # The version of the clang executable to use
-export CLANG_VERSION=4.0
+CLANG_VERSION=4.0
+
+# The default branch to compare against
+DEFAULT_BRANCH="master"
 
 # The current directory
 CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Check that we got exactly one argument
-if [ "$#" -ne 1 ]; then
-    echo "This program takes 1 argument (the name of the branch to compare against)."
+# Check for arguments
+if [ "$#" -eq 1 ]; then
+    # If there is one arg, then that is the target branch to compare against
+    # (to figure out what changes were made and need to be formatted)
+    TARGET_BRANCH=$1
+elif [ "$#" -eq 0 ]; then
+    # If there are no args, then just compare against the default branch 
+    # (to figure out what changes were made and need to be formatted)
+    TARGET_BRANCH=$DEFAULT_BRANCH
+else
+    echo "This program takes at most one argument"
+    echo "If one argument is given, it will be treated as the name of the branch to compare against to find changes to format"
+    echo "If no arguments are given, then the default branch (currently: $DEFAULT_BRANCH) will be used"
     exit 1
 fi
-TARGET_BRANCH=$1
 
 # Fix formatting on all changes between this branch and the target branch
 OUTPUT="$($CURR_DIR/git-clang-format --binary $CURR_DIR/clang-format-$CLANG_VERSION --commit $TARGET_BRANCH)"
