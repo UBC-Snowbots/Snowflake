5093f196336d93b0c2981522fd0b492b8e55cc4b
Gareth Ellis <gareth.ellis0@gmail.com>
2018-01-13 10:10:56 -0800

    added basic message types for mapping

---
+++ src/mapping_igvc/include/ObstacleManager.h
@@ -0,0 +1,17 @@
+/*
+ * Created By: Gareth Ellis
+ * Created On: January 9, 2018
+ * Description: TODO
+ */
+
+#ifndef MAPPING_IGVC_OBSTACLEMANAGER_H
+#define MAPPING_IGVC_OBSTACLEMANAGER_H
+
+#include <ConeObstacle.h>
+
+class ObstacleManager {
+    // Add obstacle (should have versions for polynomial line and cone)
+};
+
+
+#endif //MAPPING_IGVC_OBSTACLEMANAGER_H
---
+++ src/mapping_igvc/src/ObstacleManager.cpp
@@ -0,0 +1,7 @@
+/*
+ * Created By: Gareth Ellis
+ * Created On: January 9, 2018
+ * Description: TODO
+ */
+
+#include <ObstacleManager.h>
