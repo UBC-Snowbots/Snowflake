0b4abe636228db602475985d9b766205b3f8dadc
Gareth Ellis <garethellis0@users.noreply.github.com>
2018-01-20 15:18:25 -0800

    Added arduino to the setup script (#265)
    * added arduino to the `get_started.sh` script
    
    * switched paths into variables and fixed the file patching
    
    * uncommented download stuff and removed arduino via apt-get

--- get_started.sh
+++ get_started.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 # Directory this file was executed from
-echo "================================================================"
+echo "========================================================================"
 echo "Starting first time installation and setup, please wait"
-echo "these downloads can take a while if you're on slow internet."
-echo "================================================================"
+echo "these downloads can take a while if you have a slow internet connection."
+echo "========================================================================"
 
 # The current directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 
 echo "================================================================"
 echo "Setting up your shell config files"
@@ -99,6 +98,58 @@ cd /usr/share/clion*
 echo "Linking CLion"
 sudo ln -s -f /usr/share/clion*/bin/clion.sh /usr/local/bin/clion
 
+###################
+# Install Arduino #
+###################
+
+echo "================================================================"
+echo "Installing Arduino"
+echo "================================================================"
+ARDUINO_INSTALL_DIR=/usr/share
+ARDUINO_DIR_NAME=arduino
+ARDUINO_ARCHIVE_NAME=$ARDUINO_DIR_NAME.tar.xz
+
+echo "Removing system version of arduino"
+sudo apt-get remove arduino
+
+echo "Downloading Arduino"
+sudo rm -rf /tmp/$ARDUINO_DIR_NAME
+sudo rm -rf /tmp/$ARDUINO_ARCHIVE_NAME
+wget http://downloads.arduino.cc/arduino-1.8.5-linux64.tar.xz -O /tmp/$ARDUINO_ARCHIVE_NAME
+
+echo "Extracting Arduino"
+mkdir /tmp/$ARDUINO_DIR_NAME
+tar xpvf /tmp/$ARDUINO_ARCHIVE_NAME -C /tmp/$ARDUINO_DIR_NAME --strip-components 1 > /dev/null
+sudo rm -rf $ARDUINO_INSTALL_DIR/$ARDUINO_DIR_NAME
+sudo mv /tmp/$ARDUINO_DIR_NAME $ARDUINO_INSTALL_DIR
+
+echo "Running the Arduino install script"
+$ARDUINO_INSTALL_DIR/$ARDUINO_DIR_NAME/install.sh
+
+echo "Linking Arduino"
+sudo rm /usr/local/bin/arduino
+sudo ln -s -f $ARDUINO_INSTALL_DIR/$ARDUINO_DIR_NAME/arduino /usr/local/bin/arduino
+
+echo "Applying Snowbots modifications to core Arduino files"
+
+## Add the `flushRX` function. For context, see issue #176
+
+# Add the implementation to the `.cpp` file
+# Remove the last line (the final `#endif`) so we're inside the guards
+sed -i '$ d' $ARDUINO_INSTALL_DIR/$ARDUINO_DIR_NAME/hardware/arduino/avr/cores/arduino/HardwareSerial.cpp
+# Append the implementation (and the `#endif` we just removed)
+sudo tee -a $ARDUINO_INSTALL_DIR/$ARDUINO_DIR_NAME/hardware/arduino/avr/cores/arduino/HardwareSerial.cpp > /dev/null <<'TXT'
+void HardwareSerial::flushRX()
+{
+  _rx_buffer_head = _rx_buffer_tail;
+}
+#endif
+TXT
+
+# Add the declaration to the `.h` file 
+# Add it after the normal `flush` function in the `HardwareSerial` class
+sudo sed -i '/virtual void flush/a virtual void flushRX(void);' \
+/usr/share/arduino/hardware/arduino/avr/cores/arduino/HardwareSerial.h
 
 ##############################
 # Install Other Dependencies #
