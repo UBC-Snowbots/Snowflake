8cda6a913c50d5f4e1024e81829ec686a2c21455
Vincent Yuan <yuanv@alumni.ubc.ca>
2018-02-05 21:57:14 -0800

    Added udev copy rule for Phidgets

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -47,6 +47,16 @@ rosinstall .
 echo "bash $CURR_DIR/setup.sh" >> ~/.bashrc
 
 echo "================================================================"
+echo "Installing Project Dependent ROS packages."
+echo "================================================================"
+
+# Setup udev rules
+sudo cp /usr/share/ros/phidgets_api/share/udev/99-phidgets.rules /etc/udev/rules.d
+echo "Phidgets udev rules have been copied to /etc/udev/rules.d"
+# The reason the script isn't used is because stupid bash/sh doesn't know which directory it's in
+# The actual script that comes with phidget is under /usr/share/ros/phidgets_api/share/setup-udev.sh
+
+echo "================================================================"
 echo "Finished Installing Utilities"
 echo "================================================================"
 
