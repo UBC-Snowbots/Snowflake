3c7eac429cc8775b2501d7345d3dd878db4bec77
William Gu <williamgu2011@hotmail.com>
2018-02-10 16:10:44 -0800

    Small number adjustments

--- src/pathfinding_igvc/test/path_finding_rostest.cpp
+++ src/pathfinding_igvc/test/path_finding_rostest.cpp
@@ -26,7 +26,6 @@ public:
 };
 
 TEST_F(PathFindingTest, testStraightPathFinding){//Note that without given orientation/position, initial is 0,0 (center facing x)
-
     //Construct a path message to send to the test node
     //Path is currently a straight line
     nav_msgs::Path path_msg;
@@ -53,7 +52,7 @@ TEST_F(PathFindingTest, testStraightPathFinding){//Note that without given orien
     EXPECT_FLOAT_EQ(M_PI/4, turn_rate);
 }
 
-TEST_F(PathFindingTest, testCurvedPathFinding){//Note that without given orientation/position, initial is 0,0 (center facing x)
+TEST_F(PathFindingTest, testCurvedPathFinding){//Note that without given orientation/position, initial is 0,0 (center, facing x)
 
     //Construct a path message to send to the test node
     //Path is currently a straight line
@@ -86,9 +85,41 @@ TEST_F(PathFindingTest, testCurvedPathFinding){//Note that without given orienta
     EXPECT_FLOAT_EQ(M_PI/4, turn_rate);
 }
 
+TEST_F(PathFindingTest, testSharpUTurn){//Note that without given orientation/position, initial is 0,0 (center, facing x)
+    //Construct a path message to send to the test node
+    //Path is currently a straight line
+    nav_msgs::Path path_msg;
+    std::vector<geometry_msgs::PoseStamped> poses;
+    for (int i=1; i<10; i++){
+        geometry_msgs::PoseStamped pose;
+        pose.pose.position.y = i;
+        if (i<=3){
+            pose.pose.position.x = i;
+        }
+        else{
+            pose.pose.position.x = 6-i;
+        }
+        poses.push_back(pose);
+    }
+    path_msg.poses = poses;
+
+    test_publisher.publish(path_msg);
+
+    // Wait for the message to get passed around
+    ros::Rate loop_rate(1);
+    loop_rate.sleep();
+
+    // spinOnce allows ros to actually process your callbacks
+    // for the curious: http://answers.ros.org/question/11887/significance-of-rosspinonce/
+    ros::spinOnce();
+
+    EXPECT_FLOAT_EQ(0.875, speed);
+    EXPECT_FLOAT_EQ(M_PI/4, turn_rate);
+}
+
 int main(int argc, char **argv) {
     // !! Don't forget to initialize ROS, since this is a test within the ros framework !!
     ros::init(argc, argv, "path_finding_rostest");
-    ::testing::InitGoogleTest(&argc, argv);
+    testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
