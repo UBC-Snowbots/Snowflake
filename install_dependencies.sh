6acf406b650e6debabf1d2d132f30626e832d8bd
Vincent <yuanv@alumni.ubc.ca>
2018-02-26 20:03:38 -0800

    Oops forgot about udev rules

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -50,7 +50,7 @@ echo "Installing Udev rules for phidgets"
 echo "================================================================"
 
 # Setup udev rules
-sudo cp /usr/share/ros/phidgets_api/share/udev/99-phidgets.rules /etc/udev/rules.d
+sudo cp extended_pkg/phidgets_api/share/udev/99-phidgets.rules /etc/udev/rules.d
 echo "Phidgets udev rules have been copied to /etc/udev/rules.d"
 # The reason the script isn't used is because stupid bash/sh doesn't know which directory it's in
 # The actual script that comes with phidget is under /usr/share/ros/phidgets_api/share/setup-udev.sh
