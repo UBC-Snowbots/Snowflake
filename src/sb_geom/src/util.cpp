89df77861e333120afdc8189369b377b0868cecd
Gareth Ellis <gareth.ellis0@gmail.com>
2018-03-21 14:12:13 -0700

    fixed minDistanceFromPointToPolynomialSegment to optimize actual distance instead of just f(x)

--- src/sb_geom/src/util.cpp
+++ src/sb_geom/src/util.cpp
@@ -15,19 +15,38 @@ double sb_geom::minDistanceFromPointToPolynomialSegment(
         Point2D point, PolynomialSegment line, uintmax_t max_iter) {
     std::vector<double>& coeffecients = line.coefficients();
 
-    // Define a function that returns f(x), f'(x), f''(x)
+    // Define a function of the distance from the point to the line that we can optimize
+    // to find the shortest distance. For `halley_iterate` to work, we need to return the function
+    // value, along with it's first and second derivatives
     std::function<std::tuple<double, double, double>(double x)> f =
     [](double x) {
-        double fx = line(x);
-        double dx = line.deriv1st(x);
-        double d2x = line.deriv2nd(x);
-        return std::make_tuple(fx, dx, d2x);
+        double x0 = point.x();
+        double y0 = point.y();
+
+        double f_x = line(x);
+        double f_dx = line.deriv1st(x);
+        double f_d2x = line.deriv2nd(x);
+
+        // g(x) is the function representing the distance from the point to the polynomial line
+        // for g'(x) see https://www.wolframalpha.com/input/?i=d%2Fdx+sqrt((f(x)-y_0)%5E2%2B(x-x_0)%5E2)
+
+        // g(x)
+        double g_x = std::sqrt(std::pow(f_x - point.y(), 2) + std::pow(x - point.x(), 2));
+        // g'(x): see https://www.wolframalpha.com/input/?i=d%2Fdx+sqrt((f(x)-y_0)%5E2%2B(x-x_0)%5E2)
+        double g_dx = (2*(f_x-y0)*f_dx + 2*(x - x0))/
+                (2 * std::sqrt(std::pow(f_x-y0,2) + std::pow(x-x0,2)));
+        // g''(x): see https://www.wolframalpha.com/input/?i=2nd+derivative+sqrt((f(x)-y_0)%5E2%2B(x-x_0)%5E2)+with+respect+to+x
+        double g_d2x =
+                (2*(f_x-y0)*f_d2x + 2*std::pow(f_dx,2) + 2)/
+                (2 * std::sqrt(std::pow(f_x-y0,2) + std::pow(x-x0,2)))
+                -
+                std::pow(2*(f_x-y0)*f_dx + 2*(x-x0), 2)/
+                (4*std::pow(std::pow(f_x-y0,2) + std::pow(x-x0,2),3/2));
+        return std::make_tuple(g_x, g_dx, g_d2x);
     };
 
-    // TODO: better initial_guess
-
-    // Just initial_guess one of the end points of the line
-    double initial_guess = line.x_min();
+    // Just initial_guess the mid point of the line
+    double initial_guess = (line.x_min() + line.x_max())/2;
     // Maximum possible binary digits accuracy for doubles
     // digits used to control how accurate to try to make the result.
     const int digits = std::numeric_limits<double>::digits;
