6e1bbc6a813f69cd4b7e5fa2460ab11ab05ee66d
Gareth Ellis <gareth.ellis0@gmail.com>
2018-03-27 11:39:15 -0700

    remove unused function for finding distance from point to polynomial

--- src/sb_geom/include/sb_geom/utils.h
+++ src/sb_geom/include/sb_geom/utils.h
@@ -16,22 +16,6 @@
 #include "sb_geom/Spline.h"
 
 namespace sb_geom {
-    // TODO: Test me
-    // TODO: Do we need this function any more? Check if we do and delete if not
-    // TODO: This will only find local minima.... so it's wrong, use dlib::find_min_global if needed
-    /**
-     * Computes the shortest distance between a given point and polynomial line segment
-     *
-     * Uses Halley Iteration
-     * @param point TODO?
-     * @param line TODO?
-     * @param max_iter the maximum number of iterations to perform, more iterations
-     * will lead to more accurate results, but increases computation time
-     * @return the minimum distance between the point and polynomial line
-     */
-    double minDistanceFromPointToPolynomialSegment(
-            Point2D point, PolynomialSegment line, uintmax_t max_iter = 10);
-
     /**
      * Computes the minimum distance between two given splines
      *
@@ -72,7 +56,6 @@ namespace sb_geom {
      */
     double findClosestPointOnSplineToPoint(Spline spline, Point2D point, unsigned int max_iter = 10);
 
-    // TODO: Test me
     /**
      * Find a set of interpolation points in a Polynomial Segment
      *
--- src/sb_geom/src/utils.cpp
+++ src/sb_geom/src/utils.cpp
@@ -22,58 +22,6 @@
 
 using namespace sb_geom;
 
-double sb_geom::minDistanceFromPointToPolynomialSegment(
-        Point2D point, PolynomialSegment line, uintmax_t max_iter) {
-    std::vector<double>& coefficients = line.coefficients();
-
-    // Define a function of the distance from the point to the line that we can optimize
-    // to find the shortest distance. For `halley_iterate` to work, we need to return the function
-    // value, along with it's first and second derivatives
-    std::function<std::tuple<double, double, double>(double x)> f =
-    [&](double x) {
-        double x0 = point.x();
-        double y0 = point.y();
-
-        double f_x = line(x);
-        double f_dx = line.deriv(x, 1);
-        double f_d2x = line.deriv(x, 2);
-
-        // g(x) is the function representing the distance from the point to the polynomial line
-        // for g'(x) see https://www.wolframalpha.com/input/?i=d%2Fdx+sqrt((f(x)-y_0)%5E2%2B(x-x_0)%5E2)
-
-        // g(x)
-        double g_x = std::sqrt(std::pow(f_x - point.y(), 2) + std::pow(x - point.x(), 2));
-        // g'(x): see https://www.wolframalpha.com/input/?i=d%2Fdx+sqrt((f(x)-y_0)%5E2%2B(x-x_0)%5E2)
-        double g_dx = (2*(f_x-y0)*f_dx + 2*(x - x0))/
-                (2 * std::sqrt(std::pow(f_x-y0,2) + std::pow(x-x0,2)));
-        // g''(x): see https://www.wolframalpha.com/input/?i=2nd+derivative+sqrt((f(x)-y_0)%5E2%2B(x-x_0)%5E2)+with+respect+to+x
-        double g_d2x =
-                (2*(f_x-y0)*f_d2x + 2*std::pow(f_dx,2) + 2)/
-                (2 * std::sqrt(std::pow(f_x-y0,2) + std::pow(x-x0,2)))
-                -
-                std::pow(2*(f_x-y0)*f_dx + 2*(x-x0), 2)/
-                (4*std::pow(std::pow(f_x-y0,2) + std::pow(x-x0,2),3/2));
-        return std::make_tuple(g_x, g_dx, g_d2x);
-    };
-
-    // Just initial_guess the mid point of the line
-    double initial_guess = (line.x_min() + line.x_max())/2;
-    // Maximum possible binary digits accuracy for doubles
-    // digits used to control how accurate to try to make the result.
-    const int digits = std::numeric_limits<double>::digits;
-    // Accuracy triples with each step, so stop when just
-    // over one third of the digits are correct.
-    int get_digits = static_cast<int>(digits * 0.4);
-
-    // Find the x value of the closest point on f(x)
-    double best_x = boost::math::tools::halley_iterate(
-            f, initial_guess, line.x_min(), line.x_max(), get_digits, max_iter);
-
-    // Calculate the distance between the given point and the closest point on f(x)
-    double dx = point.x() - best_x;
-    double dy = point.y() - line(best_x);
-    return std::sqrt(std::pow(dx, 2) + std::pow(dy, 2));
-}
 
 double sb_geom::minDistanceBetweenSplines(Spline s1, Spline s2, unsigned int max_iter) {
     // Define a function for the distance between the two splines
