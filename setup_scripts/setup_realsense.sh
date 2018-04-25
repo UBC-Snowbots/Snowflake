a12dc1ed4cc624026f68f00ef9c8854c0993dce2
valrat <leri.ratu@gmail.com>
2018-04-25 13:21:21 -0700

    add librealsense2 explicitely in install

--- setup_scripts/setup_realsense.sh
+++ setup_scripts/setup_realsense.sh
@@ -12,10 +12,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
  echo 'deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo xenial main' | sudo tee /etc/apt/sources.list.d/realsense-public.list
 sudo apt-key adv --keyserver keys.gnupg.net --recv-key 6F3EFCDE
 sudo apt-get update
-sudo apt-get install librealsense2-dkms
-sudo apt-get install librealsense2-utils
-sudo apt-get install librealsense2-dev
-sudo apt-get install librealsense2-dbg
+sudo apt-get install librealsense2-dkms librealsense2-utils librealsense2-dev librealsense2-dbg librealsense2
 
 
 echo "================================================================"
