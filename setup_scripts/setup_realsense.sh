e78aceef217a47e0c9aa86e115c8b9f12148c887
valrat <leri.ratu@gmail.com>
2018-04-27 15:08:13 -0700

    added reasoning for setup_realsense

--- setup_scripts/setup_realsense.sh
+++ setup_scripts/setup_realsense.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# Directory this file was executed from
+# This file installs all the dependencies required by the realsense2_camera
+# ROS package from intel's repository. The package supports running the
+# D415 and D435 realsense cameras.
 echo "================================================================"
 echo "Configuring realsense repository and drivers."
 echo "================================================================"
-
-# The current directory
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 
  echo 'deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo xenial main' | sudo tee /etc/apt/sources.list.d/realsense-public.list
 sudo apt-key adv --keyserver keys.gnupg.net --recv-key 6F3EFCDE
