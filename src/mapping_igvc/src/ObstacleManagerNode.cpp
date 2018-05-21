fda68ddaeb60aacce36f688d92e2e407995f171d
Gareth Ellis <gareth.ellis0@gmail.com>
2018-05-21 01:59:09 -0700

    fixed failing tests

--- src/mapping_igvc/src/ObstacleManagerNode.cpp
+++ src/mapping_igvc/src/ObstacleManagerNode.cpp
@@ -11,7 +11,8 @@
 ObstacleManagerNode::ObstacleManagerNode(int argc, char **argv, std::string node_name) :
     // Since there is no default constructor for `ObstacleManager`, assign it some random
     // values for now (it will be overwritten later in this constructor)
-    obstacle_manager(1,1,1,1)
+    obstacle_manager(1,1,1,1),
+    occ_grid_seq(0)
 {
     // Setup NodeHandles
     ros::init(argc, argv, node_name);
@@ -19,7 +20,7 @@ ObstacleManagerNode::ObstacleManagerNode(int argc, char **argv, std::string node
     ros::NodeHandle private_nh("~");
 
     // Get Params
-    double cone_merging_tolerance, line_merging_tolerance, obstacle_inflation_buffer, occ_grid_cell_size;
+    double cone_merging_tolerance, line_merging_tolerance, obstacle_inflation_buffer, occ_grid_cell_size, occ_grid_generation_rate;
     int line_merging_max_iters, closest_line_max_iters;
     SB_getParam(private_nh, "cone_merging_tolerance", cone_merging_tolerance, 0.3);
     SB_getParam(private_nh, "line_merging_tolerance", line_merging_tolerance, 0.3);
@@ -28,6 +29,7 @@ ObstacleManagerNode::ObstacleManagerNode(int argc, char **argv, std::string node
     SB_getParam(private_nh, "line_merging_max_iters", line_merging_max_iters, 10);
     SB_getParam(private_nh, "closest_line_max_iters", closest_line_max_iters, 15);
     SB_getParam(private_nh, "occ_grid_frame", occ_grid_frame, std::string("map"));
+    SB_getParam(private_nh, "occ_grid_generation_rate", occ_grid_generation_rate, 1.0);
 
     // Setup the Obstacle Manager
     obstacle_manager = ObstacleManager(
@@ -36,8 +38,7 @@ ObstacleManagerNode::ObstacleManagerNode(int argc, char **argv, std::string node
             obstacle_inflation_buffer,
             occ_grid_cell_size,
             (unsigned int)line_merging_max_iters,
-            (unsigned int)closest_line_max_iters,
-            occ_grid_frame
+            (unsigned int)closest_line_max_iters
     );
 
     // Setup Subscriber(s)
@@ -47,6 +48,13 @@ ObstacleManagerNode::ObstacleManagerNode(int argc, char **argv, std::string node
     // Setup Publisher(s)
     std::string topic = private_nh.resolveName("occ_grid");
     occ_grid_publisher = private_nh.advertise<nav_msgs::OccupancyGrid>(topic, 10);
+
+    // Setup the timer that will publish the occupancy grid at a set frequency
+    // NOTE: This acts similairly to a callback, expect that instead of being
+    // triggered when we receive a message, it is triggered after a set period
+    // of time
+    ros::Duration occ_grid_generation_persion(1 / occ_grid_generation_rate);
+
 }
 
 void ObstacleManagerNode::coneObstacleCallback(const mapping_igvc::ConeObstacle::ConstPtr &cone_msg) {
@@ -57,5 +65,16 @@ void ObstacleManagerNode::coneObstacleCallback(const mapping_igvc::ConeObstacle:
 void ObstacleManagerNode::lineObstacleCallback(const mapping_igvc::LineObstacle::ConstPtr &line_msg) {
     // TODO (Part 4): Translate to `occ_grid_frame` before adding to obstacle manager
     obstacle_manager.addObstacle(*line_msg);
+}
+
+void ObstacleManagerNode::publishGeneratedOccupancyGrid(const ros::TimerEvent &timer_event) {
+    nav_msgs::OccupancyGrid occ_grid = obstacle_manager.generateOccupancyGrid();
+
+    occ_grid.header.stamp = ros::Time::now();
+    occ_grid.header.frame_id = occ_grid_frame;
+    occ_grid.header.seq = occ_grid_seq;
+    occ_grid_seq++;
+
+    occ_grid_publisher.publish(occ_grid);
 }
 
