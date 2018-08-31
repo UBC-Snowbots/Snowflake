f8a900b5d298dbb61e6e517b8d91b95867465c3a
Gareth Ellis <gareth.ellis0@gmail.com>
2018-08-31 13:34:03 -0600

    Commented Out realsense Installation Due to ROS Melodic Compatability Issues:

--- setup_scripts/setup_realsense.sh
+++ setup_scripts/setup_realsense.sh
@@ -9,6 +9,7 @@ echo "================================================================"
 
 sudo apt-key adv --keyserver keys.gnupg.net --recv-key C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C8B3A55A6F3EFCDE
 sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo bionic main" -u
+sudo rm -f /etc/apt/sources.list.d/realsense-public.list
 sudo apt-get update
 sudo apt-get install -y librealsense2-dkms librealsense2-utils librealsense2-dev librealsense2-dbg 
 
