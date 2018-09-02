e9a4dd2e6aa4808d916db337c3a171f9b5adeb84
Gareth Ellis <gareth.ellis0@gmail.com>
2018-09-02 09:58:37 -0600

    Fixes for CI

--- setup_scripts/setup_realsense.sh
+++ setup_scripts/setup_realsense.sh
@@ -7,6 +7,7 @@ echo "================================================================"
 echo "Configuring realsense repository and drivers."
 echo "================================================================"
 
+sudo apt-get install -y software-properties-common
 sudo apt-key adv --keyserver keys.gnupg.net --recv-key C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C8B3A55A6F3EFCDE
 sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo bionic main" -u
 sudo rm -f /etc/apt/sources.list.d/realsense-public.list
