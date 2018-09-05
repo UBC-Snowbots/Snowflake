e6677c3cc8efe03c69274c5094d588d95982249a
Gareth Ellis <gareth.ellis0@gmail.com>
2018-09-05 14:38:06 -0700

    Change Manual Installation For libdlib-dev to a rosdep

--- setup_scripts/install_dependencies.sh
+++ setup_scripts/install_dependencies.sh
@@ -49,12 +49,6 @@ rosdep install --from-paths \
     $CURR_DIR/../src/external_pkgs/navigation \
     --ignore-src --rosdistro melodic --skip-keys=librealsense2 -y 
 
-# We're currently waiting for a PR to go through to add this as an 
-# official rosdep (https://github.com/ros/rosdistro/pull/19012). 
-# Once that goes through we can (and should) delete this and uncomment
-# the libdlib-dev rosdep in the `sb_geom` `package.xml` file 
-sudo apt-get install -y libdlib-dev
-
 echo "================================================================"
 echo "Installing other dependencies specified by our packages"
 echo "================================================================"
