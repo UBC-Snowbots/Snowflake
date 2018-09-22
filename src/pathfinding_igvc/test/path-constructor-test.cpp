c5a979a166d74e469f5e9211e54a32e40cbeac25
mingyo <mingyo.kim3@gmail.com>
2018-09-22 13:40:03 -0700

    uncomment tests

--- src/pathfinding_igvc/test/path-constructor-test.cpp
+++ src/pathfinding_igvc/test/path-constructor-test.cpp
@@ -9,47 +9,51 @@
 #include <gtest/gtest.h>
 
 // TODO: Uncomment and fix, see issue #339
-// TEST(PathConstructor, TestConstructPath) {
-//    /* origin of OccupancyGrid */
-//    // initialize origin of occupancy grid
-//    geometry_msgs::Pose origin =
-//    PathFinderTestUtils::constructPose(3.0, 3.0, 0.0);
-//
-//    /* mapMetaData of OccupancyGrid */
-//    // initialize mapMetaData
-//    nav_msgs::MapMetaData map_meta_data;
-//    map_meta_data.resolution = 2.0;
-//    map_meta_data.width      = 2;
-//    map_meta_data.height     = 2;
-//    // add origin to mapMetaData
-//    map_meta_data.origin = origin;
-//
-//    /* OccupancyGridAdapter */
-//    OccupancyGridAdapter occupancy_grid_adapter =
-//    OccupancyGridAdapter(map_meta_data);
-//
-//    /* first point in path*/
-//    AStar::GridPoint point1(-99 - sqrt(3), -99 - 1);
-//
-//    /* second point in path*/
-//    AStar::GridPoint point2(-99, -99);
-//
-//    /* add points to path */
-//    std::stack<AStar::GridPoint> points;
-//    points.push(point1);
-//    points.push(point2);
-//
-//    nav_msgs::Path path =
-//    PathConstructor(occupancy_grid_adapter).constructPath(points);
-//
-//    tf::Quaternion q1;
-//    tf::quaternionMsgToTF(path.poses[0].pose.orientation, q1);
-//
-//    /* we lose resolution by converting a point into a grid, so allow more
-//    error
-//     */
-//    EXPECT_NEAR(q1.getAngle(), M_PI + M_PI / 6, 0.5);
-//}
+ TEST(PathConstructor, TestConstructPath) {
+    /* origin of OccupancyGrid */
+    // initialize origin of occupancy grid
+    geometry_msgs::Pose origin =
+    PathFinderTestUtils::constructPose(3.0, 3.0, 0.0);
+
+    /* mapMetaData of OccupancyGrid */
+    // initialize mapMetaData
+    nav_msgs::MapMetaData map_meta_data;
+    map_meta_data.resolution = 2.0;
+    map_meta_data.width      = 2;
+    map_meta_data.height     = 2;
+    // add origin to mapMetaData
+    map_meta_data.origin = origin;
+
+    /* OccupancyGridAdapter */
+    OccupancyGridAdapter *occupancy_grid_adapter =
+    new OccupancyGridAdapter(map_meta_data);
+
+    /* first point in path*/
+    AStar::GridPoint point1(-99 - sqrt(3), -99 - 1);
+
+    /* second point in path*/
+    AStar::GridPoint point2(-99, -99);
+
+    /* add points to path */
+    std::stack<AStar::GridPoint> points;
+    points.push(point1);
+    points.push(point2);
+
+    PathConstructor path_constructor(occupancy_grid_adapter);
+    nav_msgs::Path path = path_constructor.constructPath(points);
+
+    tf::Quaternion q1;
+    tf::quaternionMsgToTF(path.poses[0].pose.orientation, q1);
+
+    /* we lose resolution by converting a point into a grid, so allow more
+    error
+     */
+    EXPECT_NEAR(q1.getAngle(), M_PI + M_PI / 6, 0.5);
+    EXPECT_FLOAT_EQ(path.poses[0].pose.position.x, occupancy_grid_adapter->convertFromGridToMapPoint(point2).x);
+    EXPECT_FLOAT_EQ(path.poses[0].pose.position.y, occupancy_grid_adapter->convertFromGridToMapPoint(point2).y);
+    EXPECT_FLOAT_EQ(path.poses[1].pose.position.x, occupancy_grid_adapter->convertFromGridToMapPoint(point1).x);
+    EXPECT_FLOAT_EQ(path.poses[1].pose.position.y, occupancy_grid_adapter->convertFromGridToMapPoint(point1).y);
+}
 
 int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
