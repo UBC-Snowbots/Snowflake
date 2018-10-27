47871c37d2bb9277b9dbfef0ad98a3afae0a49a4
Gareth Ellis <gareth.ellis0@gmail.com>
2018-10-27 10:55:37 -0700

    setup_realsense fixes

--- setup_scripts/setup_realsense.sh
+++ setup_scripts/setup_realsense.sh
@@ -16,14 +16,13 @@ echo "================================================================"
 
 
 echo 'deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo bionic main' || sudo tee /etc/apt/sources.list.d/realsense-public.list
-- sudo apt-key adv --keyserver keys.gnupg.net --recv-key C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keys.gnupg.net:80 --recv-key C8B3A55A6F3EFCDE
+sudo apt-key adv --keyserver keys.gnupg.net --recv-key C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keys.gnupg.net:80 --recv-key C8B3A55A6F3EFCDE
 sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo bionic main"
 sudo apt-get update -qq
 sudo apt-get install librealsense2-dkms --allow-unauthenticated -y 
 sudo apt-get install librealsense2-dev --allow-unauthenticated -y
 sudo apt-get install librealsense2-utils --allow-unauthenticated -y
-sudo apt-get install librealsense2-dpg --allow-unauthenticated -y
-
+sudo apt-get install librealsense2-dbg --allow-unauthenticated -y
 
 
 echo "================================================================"
