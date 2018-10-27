8b813b830429fcaf9eb438bee88d0cba40c4fbf4
Gareth Ellis <gareth.ellis0@gmail.com>
2018-10-27 13:24:22 -0700

    Reverted changes to setup_realsense

--- setup_scripts/setup_realsense.sh
+++ setup_scripts/setup_realsense.sh
@@ -7,23 +7,12 @@ echo "================================================================"
 echo "Configuring realsense repository and drivers."
 echo "================================================================"
 
-#sudo apt-get install -y software-properties-common
-#sudo apt-key adv --keyserver keys.gnupg.net --recv-key C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C8B3A55A6F3EFCDE
-#sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo bionic main" -u
-#sudo rm -f /etc/apt/sources.list.d/realsense-public.list
-#sudo apt-get update -y
-#sudo apt-get install -y librealsense2-dkms librealsense2-utils librealsense2-dev=2.16.1-0\~realsense0.13 librealsense2-dbg 
-
-
 sudo apt-get install -y software-properties-common
-echo 'deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo bionic main' || sudo tee /etc/apt/sources.list.d/realsense-public.list
-sudo apt-key adv --keyserver keys.gnupg.net --recv-key C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keys.gnupg.net:80 --recv-key C8B3A55A6F3EFCDE
-sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo bionic main"
-sudo apt-get update -qq
-sudo apt-get install librealsense2-dkms --allow-unauthenticated -y 
-sudo apt-get install librealsense2-dev --allow-unauthenticated -y
-sudo apt-get install librealsense2-utils --allow-unauthenticated -y
-sudo apt-get install librealsense2-dbg --allow-unauthenticated -y
+sudo apt-key adv --keyserver keys.gnupg.net --recv-key C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C8B3A55A6F3EFCDE
+sudo add-apt-repository "deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo bionic main" -u
+sudo rm -f /etc/apt/sources.list.d/realsense-public.list
+sudo apt-get update -y
+sudo apt-get install -y librealsense2-dkms librealsense2-utils librealsense2-dev librealsense2-dbg 
 
 
 echo "================================================================"
