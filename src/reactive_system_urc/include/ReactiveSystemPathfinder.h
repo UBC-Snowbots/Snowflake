3ffe4d5d2cc23a826e7c9c19aaf83fe883a20657
William Gu <williamgu2011@hotmail.com>
2019-01-05 13:49:50 -0800

    Added new branch for alternative reactive system method

--- src/reactive_system_urc/include/ReactiveSystemPathfinder.h
+++ src/reactive_system_urc/include/ReactiveSystemPathfinder.h
@@ -8,8 +8,6 @@
 #ifndef PROJECT_REACTIVESYSTEMPATHFINDER_H
 #define PROJECT_REACTIVESYSTEMPATHFINDER_H
 
-#include <DBScanPoly.h>
-#include <ConvexHull.h>
 #include <iostream>
 #include <math.h>
 #include <std_msgs/Float64.h>
@@ -28,55 +26,12 @@ public:
 
     /**
      * Entrypoint for pathfinding algorithm
-     * risk_threshold refers to the minimum risk score for polygons to be placed in a cluster
-     * cluster_dist_tol refers to the max distance between two polygons in a cluster
+
      */
-    static nav_msgs::Path pathFinder(mapping_msgs_urc::RiskAreaArray risk_areas, geometry_msgs::Point32 curr_pos, geometry_msgs::Point32 goal_pos, double risk_threshold, double cluster_dist_tol, double point_dist_tol);
-
-
-    /**
-     * Filters risk polygons above some risk value
-     */
-    static std::vector<geometry_msgs::Polygon> filterRiskPolygons(mapping_msgs_urc::RiskAreaArray& risk_areas, double risk_threshold);
-
-
-    /**
-     * Identify adjacent and overlapping polygons and group them into clusters -> main logic is located in DBScanPoly
-     */
-    static std::vector<std::vector<geometry_msgs::Polygon>> clusterRiskPolygons(std::vector<geometry_msgs::Polygon> polygons, double cluster_dist_tol);
-
-
-    /**
-     * Converts clusters of polygons into clusters of points, removing redundant points in the process
-     */
-    static std::vector<std::vector<geometry_msgs::Point32>> polygonClustersToPoints(std::vector<std::vector<geometry_msgs::Polygon>> polygon_clusters, double point_dist_tol);
-
-
-    /**
-     * Performs convex hull algorithm on each cluster of points, returning a vector of resultant polygons (size of resultant polygons should equal number of clusters entered)
-     */
-    static std::vector<geometry_msgs::Polygon> massConvexHull(std::vector<std::vector<geometry_msgs::Point32>> point_clusters);
-
-
-    //May need to perform outward polygon offsetting/polygon expansion to account for robot size (doesn't get too close to edges)
-    //Could use a convex hull heurestic/approximation like this: http://geomalgorithms.com/a11-_hull-2.html
-    //Or could build offsetting into convex hull algorithm somehow
-
-
-    /**
-     * Constructs a path from currPos to goalPos avoiding any polygons in the way
-     */
-    static nav_msgs::Path constructPath(std::vector<geometry_msgs::Polygon> polygon_obstacles, geometry_msgs::Point32 curr_pos, geometry_msgs::Point32 goal_pos);
+    static nav_msgs::Path pathFinder(mapping_msgs_urc::RiskAreaArray risk_areas, geometry_msgs::Point32 curr_pos, geometry_msgs::Point32 goal_pos);
 
 
 
-
-    /* HELPER FUNCTIONS */
-
-    /**
-     * Returns true if points are within dist_tol (only checks x and y for fast computations)
-     */
-    static bool closePoints(geometry_msgs::Point32 p1, geometry_msgs::Point32 p2, double dist_tol);
 };
 
 #endif //PROJECT_REACTIVESYSTEMPATHFINDER_H
--- src/reactive_system_urc/src/ReactiveSystemPathfinder.cpp
+++ src/reactive_system_urc/src/ReactiveSystemPathfinder.cpp
@@ -5,93 +5,9 @@
 
 #include <ReactiveSystemPathfinder.h>
 
-nav_msgs::Path ReactiveSystemPathfinder::pathFinder(mapping_msgs_urc::RiskAreaArray risk_areas, geometry_msgs::Point32 curr_pos, geometry_msgs::Point32 goal_pos, double risk_threshold, double cluster_dist_tol, double point_dist_tol) {
+nav_msgs::Path ReactiveSystemPathfinder::pathFinder(mapping_msgs_urc::RiskAreaArray risk_areas, geometry_msgs::Point32 curr_pos, geometry_msgs::Point32 goal_pos) {
 
-    //Pipeline for obtaining path
-    std::vector<geometry_msgs::Polygon> polygons = filterRiskPolygons(risk_areas, risk_threshold);
-    std::vector<std::vector<geometry_msgs::Polygon>> clustered_polygons = clusterRiskPolygons(polygons, cluster_dist_tol);
-    std::vector<std::vector<geometry_msgs::Point32>> clustered_points = polygonClustersToPoints(clustered_polygons, point_dist_tol);
-    std::vector<geometry_msgs::Polygon> polygon_obstacles = massConvexHull(clustered_points);
-    return constructPath(polygon_obstacles, curr_pos, goal_pos);
-
-}
-
-
-std::vector<geometry_msgs::Polygon> ReactiveSystemPathfinder::filterRiskPolygons(mapping_msgs_urc::RiskAreaArray& risk_areas, double risk_threshold) {
-    std::vector<geometry_msgs::Polygon> polygons;
-    for (mapping_msgs_urc::RiskArea risk_area : risk_areas.areas){
-        if (risk_area.score.data > risk_threshold){
-            polygons.push_back(risk_area.area);
-        }
-    }
-    return polygons;
-}
-
-
-std::vector<std::vector<geometry_msgs::Polygon>> ReactiveSystemPathfinder::clusterRiskPolygons(std::vector<geometry_msgs::Polygon> polygons, double cluster_dist_tol){
-
-    return DBScanPoly::findClusters(polygons, cluster_dist_tol);
-
-}
-
-
-
-std::vector<std::vector<geometry_msgs::Point32>> ReactiveSystemPathfinder::polygonClustersToPoints(std::vector<std::vector<geometry_msgs::Polygon>> polygon_clusters, double point_dist_tol) {
-
-    std::vector<std::vector<geometry_msgs::Point32>> point_clusters;
-
-    for (std::vector<geometry_msgs::Polygon> cluster : polygon_clusters) {
-
-        std::vector<geometry_msgs::Point32> point_cluster;
-
-        for (geometry_msgs::Polygon poly : cluster) {
-            for (geometry_msgs::Point32 point: poly.points){
-
-                //Check for close enough duplicates in each cluster
-                bool duplicate_point = false;
-                for (geometry_msgs::Point32 existing_point : point_cluster){
-                    if (closePoints(existing_point, point, point_dist_tol)){
-                        duplicate_point = true;
-                        break;
-                    }
-                }
-                if (!duplicate_point)
-                    point_cluster.push_back(point);
-
-            }
-        }
-
-        point_clusters.push_back(point_cluster);
-    }
-
-    return point_clusters;
-
-}
-
-
-std::vector<geometry_msgs::Polygon> ReactiveSystemPathfinder::massConvexHull(std::vector<std::vector<geometry_msgs::Point32>> point_clusters) {
-
-    std::vector<geometry_msgs::Polygon> convex_hulls;
-
-    for (std::vector<geometry_msgs::Point32> point_cluster : point_clusters) {
-
-        geometry_msgs::Polygon convex_hull = ConvexHull::convexHull(point_cluster);
-        convex_hulls.push_back(convex_hull);
-    }
-
-    return convex_hulls;
-
-}
-
-
-nav_msgs::Path ReactiveSystemPathfinder::constructPath(std::vector<geometry_msgs::Polygon> polygon_obstacles, geometry_msgs::Point32 curr_pos, geometry_msgs::Point32 goal_pos){
-
-}
-
-
-
-bool ReactiveSystemPathfinder::closePoints (geometry_msgs::Point32 p1, geometry_msgs::Point32 p2, double dist_tol){
-
-    return abs(p1.x - p2.x) <= dist_tol && abs(p1.y - p2.y) <= dist_tol;
+    nav_msgs::Path path;
+    return path;
 
 }
