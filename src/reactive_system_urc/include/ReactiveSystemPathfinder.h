9d79485d151517fd8c4fe1925bda0112f0d912da
William Gu <williamgu2011@hotmail.com>
2019-01-05 19:31:09 -0800

    Added functions for arc calculations for arbitrary linear and angular velocities

--- src/reactive_system_urc/include/ReactiveSystemPathfinder.h
+++ src/reactive_system_urc/include/ReactiveSystemPathfinder.h
@@ -10,12 +10,12 @@
 
 #include <iostream>
 #include <math.h>
-#include <std_msgs/Float64.h>
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
 #include <nav_msgs/Path.h>
+#include <geometry_msgs/Twist.h>
 #include <geometry_msgs/Point.h>
-#include <geometry_msgs/Polygon.h>
+#include <geometry_msgs/Pose.h>
 #include <mapping_msgs_urc/RiskArea.h>
 #include <mapping_msgs_urc/RiskAreaArray.h>
 #include <mapping_msgs_urc/RiskAreaStamped.h>
@@ -28,10 +28,19 @@ public:
      * Entrypoint for pathfinding algorithm
 
      */
-    static nav_msgs::Path pathFinder(mapping_msgs_urc::RiskAreaArray risk_areas, geometry_msgs::Point32 curr_pos, geometry_msgs::Point32 goal_pos);
+    static nav_msgs::Path pathFinder(mapping_msgs_urc::RiskAreaArray risk_areas, geometry_msgs::Point curr_pos, geometry_msgs::Point goal_pos);
 
+    /**
+     * Calculates the arc trajectory given some twist command (linear and angular velocity)
+     * @return
+     */
+    static std::vector<geometry_msgs::Point> getArcTrajectory(double linear_vel, double angular_vel, double time_inc, int num_incs);
 
-
+    /**
+     * Calculate the arc point in a given arc trajectory by rotating (0,0) about some center point by angle
+     * @return
+     */
+    static geometry_msgs::Point getArcPoint(geometry_msgs::Point center, double angle);
 };
 
 #endif //PROJECT_REACTIVESYSTEMPATHFINDER_H
--- src/reactive_system_urc/src/ReactiveSystemPathfinder.cpp
+++ src/reactive_system_urc/src/ReactiveSystemPathfinder.cpp
@@ -5,9 +5,49 @@
 
 #include <ReactiveSystemPathfinder.h>
 
-nav_msgs::Path ReactiveSystemPathfinder::pathFinder(mapping_msgs_urc::RiskAreaArray risk_areas, geometry_msgs::Point32 curr_pos, geometry_msgs::Point32 goal_pos) {
+nav_msgs::Path ReactiveSystemPathfinder::pathFinder(mapping_msgs_urc::RiskAreaArray risk_areas, geometry_msgs::Point curr_pos, geometry_msgs::Point goal_pos) {
 
     nav_msgs::Path path;
     return path;
+
+}
+
+
+std::vector<geometry_msgs::Point> ReactiveSystemPathfinder::getArcTrajectory(double linear_vel, double angular_vel, double time_inc, int num_incs){
+
+    std::vector<geometry_msgs::Point> arc_trajectory;
+
+    if (abs(angular_vel) < 0.01){ //essentially a straight trajectory
+        for (double i = 0; i <= time_inc * num_incs; i += time_inc){
+            geometry_msgs::Point arc_point;
+            arc_point.x = time_inc * i * linear_vel;
+            arc_point.y = 0;
+            arc_trajectory.push_back(arc_point);
+        }
+    }
+    else{
+        geometry_msgs::Point center;
+        center.x = 0;
+        center.y = linear_vel/angular_vel;
+
+        for (double i = 0; i <= time_inc * num_incs; i += time_inc){
+            double angle = time_inc * i * angular_vel;
+            geometry_msgs::Point arc_point = ReactiveSystemPathfinder::getArcPoint(center, angle);
+            arc_trajectory.push_back(arc_point);
+        }
+
+    }
+
+    return arc_trajectory;
+}
+
+
+geometry_msgs::Point ReactiveSystemPathfinder::getArcPoint(geometry_msgs::Point center, double angle){
+
+    geometry_msgs::Point arc_point;
+    arc_point.x = center.x - center.x * cos(angle) + center.y * sin(angle);
+    arc_point.y = center.y - center.x * sin(angle) - center.y * cos(angle);
+
+    return arc_point;
 
 }
