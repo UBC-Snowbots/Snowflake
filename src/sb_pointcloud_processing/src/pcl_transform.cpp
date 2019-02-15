322334e20636473dba04376636a75c54472ba2e6
Robyn Castro <robyny3k@gmail.com>
2019-02-15 08:59:26 -0800

    Refactored zed_transform to pcl_transform and added a new param

--- src/sb_pointcloud_processing/src/pcl_transform.cpp
+++ src/sb_pointcloud_processing/src/pcl_transform.cpp
@@ -14,8 +14,12 @@
 #include <sb_utils.h>
 
 ros::Publisher pub;
-std::string output_frame;
 tf2_ros::Buffer tf_buffer;
+
+std::string output_frame;
+
+// The amount of time (s) for a new pointcloud transform to be published.
+double transform_period;
 
 void pointCloudCallback(const sensor_msgs::PointCloud2::ConstPtr& msg) {
     try {
@@ -23,29 +27,41 @@ void pointCloudCallback(const sensor_msgs::PointCloud2::ConstPtr& msg) {
         sensor_msgs::PointCloud2 output;
         // Transform the pointcloud to the requested frame
         geometry_msgs::TransformStamped tf_stamped = tf_buffer.lookupTransform(
-        msg->header.frame_id, output_frame, ros::Time(0), ros::Duration(1.0));
+        msg->header.frame_id, output_frame, ros::Time(0), ros::Duration(0.1));
+
         tf2::doTransform(*msg, output, tf_stamped);
 
         // Publish the transformed pointcloud
         pub.publish(output);
-    } catch (tf2::TransformException ex){
+
+        ros::Duration(transform_period).sleep();
+    } catch (tf2::TransformException ex) {
         ROS_WARN("%s", ex.what());
         ros::Duration(1.0).sleep();
     }
 }
 
-int main(int argc, char** argv){
+int main(int argc, char** argv) {
     ros::init(argc, argv, "pointcloud_transformer");
     ros::NodeHandle private_nh("~");
-    if (!SB_getParam(private_nh, "output_frame", output_frame)){
+    if (!SB_getParam(private_nh, "output_frame", output_frame)) {
         // Error and exit if we didn't get a frame to transform to.
-        // We need this to transform anything, and there is no reasonable default
-        ROS_ERROR("Param 'output_frame' not provided. " \
-                  "Can't  transform anything without a frame to transform it to");
+        // We need this to transform anything, and there is no reasonable
+        // default
+        ROS_ERROR(
+        "Param 'output_frame' not provided. "
+        "Can't  transform anything without a frame to transform it to");
         return 1;
     }
-    ros::Subscriber sub = private_nh.subscribe("/input_pointcloud", 1, pointCloudCallback);
-    pub = private_nh.advertise<sensor_msgs::PointCloud2>("/output_pointcloud", 1);
+
+    double default_transform_period = 0;
+    SB_getParam(
+    private_nh, "transform_period", transform_period, default_transform_period);
+
+    ros::Subscriber sub =
+    private_nh.subscribe("/input_pointcloud", 1, pointCloudCallback);
+    pub =
+    private_nh.advertise<sensor_msgs::PointCloud2>("/output_pointcloud", 1);
     tf2_ros::TransformListener tfListener(tf_buffer);
     ros::spin();
     return 0;
