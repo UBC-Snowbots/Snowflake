a025f2256ffbf9ff1e1c0331b10ef42f187cbf72
William Gu <williamgu2011@hotmail.com>
2019-07-16 20:55:48 -0700

    Added ROStests for RC broker

--- src/rc_broker/test/rc_broker_rostest.cpp
+++ src/rc_broker/test/rc_broker_rostest.cpp
@@ -41,8 +41,64 @@ public:
     }
 };
 
-TEST_F(RCBrokerTest, testEZ) {
-    EXPECT_TRUE(true);
+TEST_F(RCBrokerTest, testServerToClient) {
+    std_msgs::String msg;
+    msg.data = "Hello Client!";
+    test_publisher_server.publish(msg);
+
+    // Wait for the message to get passed around
+    ros::Rate loop_rate(1);
+    loop_rate.sleep();
+    ros::spinOnce();
+
+    EXPECT_TRUE(rcv_client_msg.data == "Hello Client!");
+}
+
+TEST_F(RCBrokerTest, testClientToServer) {
+    std_msgs::String msg;
+    msg.data = "Hello Server!";
+    test_publisher_client.publish(msg);
+
+    // Wait for the message to get passed around
+    ros::Rate loop_rate(1);
+    loop_rate.sleep();
+    ros::spinOnce();
+
+    EXPECT_TRUE(rcv_server_msg.data == "Hello Server!");
+}
+
+TEST_F(RCBrokerTest, testSimMessages) {
+    std_msgs::String msgToServer;
+    msgToServer.data = "Hello Server!";
+
+    std_msgs::String msgToClient;
+    msgToServer.data = "Hello Client!";
+
+    test_publisher_client.publish(msgToServer);
+    test_publisher_server.publish(msgToClient);
+
+    // Wait for the message to get passed around
+    ros::Rate loop_rate(1);
+    loop_rate.sleep();
+    ros::spinOnce();
+
+    EXPECT_TRUE(rcv_server_msg.data == "Hello Server!");
+    EXPECT_TRUE(rcv_client_msg.data == "Hello Client!");
+}
+
+TEST_F(RCBrokerTest, testMultiMessages) {
+
+    for (int i = 0; i < 100; i ++){
+        std_msgs::String msg;
+        msg.data = std::to_string(i);
+        test_publisher_client.publish(msg);
+
+        ros::Rate loop_rate(1);
+        loop_rate.sleep();
+        ros::spinOnce();
+
+        EXPECT_TRUE(rcv_server_msg.data == std::to_string(i));
+    }
 }
 
 int main(int argc, char** argv) {
