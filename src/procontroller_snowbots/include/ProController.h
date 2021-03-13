3b1d2feab7314c31b657e43ec44541ff353651d6
Kevin Lin <kevinlinxc@gmail.com>
2021-03-13 18:05:47 +0000

    Clang formatting

--- src/procontroller_snowbots/include/ProController.h
+++ src/procontroller_snowbots/include/ProController.h
@@ -4,7 +4,6 @@
  * Description: Simple header file for switch controller-->ROS Twist message cpp
  */
 
-
 #ifndef PROCONTROLLER_SNOWBOTS_CONTROLLER_H
 #define PROCONTROLLER_SNOWBOTS_CONTROLLER_H
 
@@ -26,36 +25,37 @@ public:
 private:
     void setup();
     void readInputs();
-    void leftJoystickX(int value); //ABS_X
-    void leftJoystickY(int value); //ABS_Y
-    void rightJoystickX(int value); //ABS_RX
-    void rightJoystickY(int value); //ABS_RY
-    void A(int value); //BTN_EAST
-    void B(int value); //BTN_SOUTH
-    void X(int value); //BTN_WEST
-    void Y(int value); //BTN_NORTH
-    void leftBumper(int value); //BTN_TL
-    void rightBumper(int value); //BTN_TR
-    void select(int value); //BTN_SELECT
-    void start(int value); //BTN_START
-    void home(int value); //BTN_MODE
-    void leftTrigger(int value); //ABS_Z
-    void rightTrigger(int value); //ABS_RZ
-    void arrowsRorL(int value); //ABS_HAT0X
-    void arrowsUorD(int value); //ABS_HAT0Y
-    void leftJoystickPress(int value); //BTN_THUMBL
-    void rightJoystickPress(int value); //BTN_THUMBR
-    tuple<double,double> publishMoveXZ(double x_new, double z_new, double x_old, double z_old);
+    void leftJoystickX(int value);      // ABS_X
+    void leftJoystickY(int value);      // ABS_Y
+    void rightJoystickX(int value);     // ABS_RX
+    void rightJoystickY(int value);     // ABS_RY
+    void A(int value);                  // BTN_EAST
+    void B(int value);                  // BTN_SOUTH
+    void X(int value);                  // BTN_WEST
+    void Y(int value);                  // BTN_NORTH
+    void leftBumper(int value);         // BTN_TL
+    void rightBumper(int value);        // BTN_TR
+    void select(int value);             // BTN_SELECT
+    void start(int value);              // BTN_START
+    void home(int value);               // BTN_MODE
+    void leftTrigger(int value);        // ABS_Z
+    void rightTrigger(int value);       // ABS_RZ
+    void arrowsRorL(int value);         // ABS_HAT0X
+    void arrowsUorD(int value);         // ABS_HAT0Y
+    void leftJoystickPress(int value);  // BTN_THUMBL
+    void rightJoystickPress(int value); // BTN_THUMBR
+    tuple<double, double>
+    publishMoveXZ(double x_new, double z_new, double x_old, double z_old);
     void publishArmXZ(double x_new, double z_new, double x_old, double z_old);
     void printState();
-    void printControllerDebug(int type,int code,int value);
-    //see documentation to changes sensitivities at runtime
+    void printControllerDebug(int type, int code, int value);
+    // see documentation to changes sensitivities at runtime
     double X_SENSITIVITY = 1.0;
     double Z_SENSITIVITY = 1.0;
     double x;
     double z;
     struct libevdev *dev = NULL;
-    enum Mode { wheels = 0, arm = 1};
+    enum Mode { wheels = 0, arm = 1 };
     Mode state;
     bool debug = false;
     ros::Publisher pubmove;
