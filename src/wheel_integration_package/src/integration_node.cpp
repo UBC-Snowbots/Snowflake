699b65fb9f7eeba3101e78905daf3c3199402a33
vijeeth19 <56468618+vijeeth19@users.noreply.github.com>
2021-04-03 13:18:15 +0530

    Update integration_node.cpp

--- src/wheel_integration_package/src/integration_node.cpp
+++ src/wheel_integration_package/src/integration_node.cpp
@@ -1,9 +1,10 @@
 /*
- * Created By: Gareth Ellis
- * Created On: July 16th, 2016
- * Description: An example node that subscribes to a topic publishing strings,
- *              and re-publishes everything it receives to another topic with
- *              a "!" at the end
+ * Created By: Vijeeth Vijhaipranith
+ * Created On: Oct 30th, 2020
+ * Description: This node subscribes to a topic publishing Geometry Twist messages that indicate the direction to move the rover in,
+ *              and publishes velocity values to the left and right wheels of the rover.
+ *              The velocity of the left wheels is published to the topic "lwheels_pub_topic"
+ *              The velocity of the right wheels is published to the topic "rwheels_pub_topic"
  */
 
 #include <IntegrationNode.h>
@@ -13,7 +14,10 @@ int main(int argc, char **argv){
     // Setup your ROS node
     std::string node_name = "integration_node";
     float max_speed = 1.0;
+    
+    //Distance between the left and right wheels
     float dist = 0.930;
+    
     // Create an instance of your class
     MyClass my_class(argc, argv, node_name, dist,  max_speed);
 
