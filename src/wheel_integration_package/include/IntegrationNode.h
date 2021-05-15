7d81cf1469cf35382badaa86f7b0b8ebaf71b406
Vijeeth Vijhaipranith <vijeethvp@gmail.com>
2021-05-15 03:48:37 -0700

    travis fixed fomatting trial 1

--- src/wheel_integration_package/include/IntegrationNode.h
+++ src/wheel_integration_package/include/IntegrationNode.h
@@ -1,10 +1,14 @@
 /*
  * Created By: Vijeeth Vijhaipranith
  * Created On: Oct 30th, 2020
- * Description: This node subscribes to a topic publishing Geometry Twist messages that indicate the direction to move the rover in,
- *              and publishes velocity values to the left and right wheels of the rover.
- *              The velocity of the left wheels is published to the topic "/integration_node/lwheels_pub_topic"
- *              The velocity of the right wheels is published to the topic "/integration_node/rwheels_pub_topic"
+ * Description: This node subscribes to a topic publishing Geometry Twist
+ * messages that indicate the direction to move the rover in,
+ *              and publishes velocity values to the left and right wheels of
+ * the rover.
+ *              The velocity of the left wheels is published to the topic
+ * "/integration_node/lwheels_pub_topic"
+ *              The velocity of the right wheels is published to the topic
+ * "/integration_node/rwheels_pub_topic"
  */
 
 #ifndef WHEEL_INTEGRATION_PACKAGE_MYNODE_H
@@ -15,19 +19,20 @@
 
 // ROS Includes
 #include <geometry_msgs/Twist.h>
-#include <std_msgs/String.h>
 #include <ros/ros.h>
+#include <std_msgs/String.h>
 
 // Snowbots Includes
 #include <sb_utils.h>
 
 class MyClass {
-public:
-    MyClass(int argc, char **argv, std::string node_name, float dist, float max_speed);
+  public:
+    MyClass(
+    int argc, char** argv, std::string node_name, float dist, float max_speed);
     float distBetweenWheels;
     float maximum_speed;
 
-private:
+  private:
     /**
      * Callback function for when a new string is received
      *
@@ -39,4 +44,4 @@ private:
     ros::Publisher lwheels_publisher;
     ros::Publisher rwheels_publisher;
 };
-#endif //WHEEL_INTEGRATION_PACKAGE_MYNODE_H
+#endif // WHEEL_INTEGRATION_PACKAGE_MYNODE_H
--- src/wheel_integration_package/src/IntegrationNode.cpp
+++ src/wheel_integration_package/src/IntegrationNode.cpp
@@ -1,37 +1,44 @@
 /*
  * Created By: Vijeeth Vijhaipranith
  * Created On: Oct 30th, 2020
- * Description: This node subscribes to a topic publishing Geometry Twist messages that indicate the direction to move the rover in,
- *              and publishes velocity values to the left and right wheels of the rover.
- *              The velocity of the left wheels is published to the topic "/integration_node/lwheels_pub_topic"
- *              The velocity of the right wheels is published to the topic "/integration_node/rwheels_pub_topic"
+ * Description: This node subscribes to a topic publishing Geometry Twist
+ * messages that indicate the direction to move the rover in,
+ *              and publishes velocity values to the left and right wheels of
+ * the rover.
+ *              The velocity of the left wheels is published to the topic
+ * "/integration_node/lwheels_pub_topic"
+ *              The velocity of the right wheels is published to the topic
+ * "/integration_node/rwheels_pub_topic"
  */
 
 #include <IntegrationNode.h>
 
-
-MyClass::MyClass(int argc, char **argv, std::string node_name, float dist, float max_speed) {
+MyClass::MyClass(
+int argc, char** argv, std::string node_name, float dist, float max_speed) {
     // Setup NodeHandles
     ros::init(argc, argv, node_name);
     ros::NodeHandle nh;
     ros::NodeHandle private_nh("~");
 
     distBetweenWheels = dist;
-    maximum_speed = max_speed;
+    maximum_speed     = max_speed;
 
     // Setup Subscriber(s)
     std::string topic_to_subscribe_to = "cmd_vel";
-    int queue_size = 10;
-    my_subscriber = nh.subscribe(topic_to_subscribe_to, queue_size, &MyClass::subscriberCallBack, this);
+    int queue_size                    = 10;
+    my_subscriber                     = nh.subscribe(
+    topic_to_subscribe_to, queue_size, &MyClass::subscriberCallBack, this);
 
     // Setup Publisher(s)
     std::string lwheels_topic = private_nh.resolveName("lwheels_pub_topic");
-    int lwheels_queue_size = 1;
-    lwheels_publisher = private_nh.advertise<geometry_msgs::Twist>(lwheels_topic, lwheels_queue_size);
+    int lwheels_queue_size    = 1;
+    lwheels_publisher         = private_nh.advertise<geometry_msgs::Twist>(
+    lwheels_topic, lwheels_queue_size);
 
     std::string rwheels_topic = private_nh.resolveName("rwheels_pub_topic");
-    int rwheels_queue_size = 1;
-    rwheels_publisher = private_nh.advertise<geometry_msgs::Twist>(rwheels_topic, rwheels_queue_size);
+    int rwheels_queue_size    = 1;
+    rwheels_publisher         = private_nh.advertise<geometry_msgs::Twist>(
+    rwheels_topic, rwheels_queue_size);
 }
 
 /*
@@ -47,45 +54,47 @@ void MyClass::subscriberCallBack(const geometry_msgs::Twist::ConstPtr& msg) {
     /* This variable is created to avoid division by 0 error */
     float zero_threshold = 0.01;
 
-    /* Calculate the linear and angular velocities required by the Pro Controller */
-    float linear_vel = maximum_speed * msg->linear.x;
+    /* Calculate the linear and angular velocities required by the Pro
+     * Controller */
+    float linear_vel  = maximum_speed * msg->linear.x;
     float angular_vel = msg->angular.z;
 
     float lwheel_vel, rwheel_vel;
 
-    /* 
-     * If the angular velocty is 0, then the rover moves straight 
-     * and we set the velocities of both of its wheels equal to the 
+    /*
+     * If the angular velocty is 0, then the rover moves straight
+     * and we set the velocities of both of its wheels equal to the
      * linear velocity required by the Pro Controller
      */
-    if(abs(angular_vel) < zero_threshold) {
+    if (abs(angular_vel) < zero_threshold) {
         lwheel_vel = linear_vel;
         rwheel_vel = linear_vel;
-    }
-    else {
+    } else {
         /* Calculate the radius of curvature of required by Pro Controller */
-        float radius_of_curvature = linear_vel/angular_vel;
+        float radius_of_curvature = linear_vel / angular_vel;
 
-        /* Calculate the velocities of the left and right wheel according to the formula on README.md */
-        rwheel_vel = angular_vel * (radius_of_curvature + distBetweenWheels/2);
-        lwheel_vel = angular_vel * (radius_of_curvature - distBetweenWheels/2);
+        /* Calculate the velocities of the left and right wheel according to the
+         * formula on README.md */
+        rwheel_vel =
+        angular_vel * (radius_of_curvature + distBetweenWheels / 2);
+        lwheel_vel =
+        angular_vel * (radius_of_curvature - distBetweenWheels / 2);
     }
 
     geometry_msgs::Twist lwheels_msg;
     geometry_msgs::Twist rwheels_msg;
 
-    /* Create Geometry Twist messages by assigning the velocities of the wheels as the linear.x component */
-    lwheels_msg.linear.x = lwheel_vel;
+    /* Create Geometry Twist messages by assigning the velocities of the wheels
+     * as the linear.x component */
+    lwheels_msg.linear.x  = lwheel_vel;
     lwheels_msg.angular.z = 0.0;
-    rwheels_msg.linear.x = rwheel_vel;
+    rwheels_msg.linear.x  = rwheel_vel;
     rwheels_msg.angular.z = 0.0;
 
-    /* Publish the created Geometry Twist messages to their appropriate topics */
+    /* Publish the created Geometry Twist messages to their appropriate topics
+     */
     lwheels_publisher.publish(lwheels_msg);
     rwheels_publisher.publish(rwheels_msg);
 
     ROS_INFO("Published messages");
 }
-
-
-
