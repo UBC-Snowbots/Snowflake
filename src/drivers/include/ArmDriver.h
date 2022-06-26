e88ca500218615ca91085ac3d7e11e96f8fd22e4
Ihsan Olawale <ihsanolawale@outlook.com>
2022-06-26 16:57:44 -0700

    Remove starter code

--- src/drivers/include/ArmDriver.h
+++ src/drivers/include/ArmDriver.h
@@ -25,25 +25,8 @@
 class ArmDriver {
 public:
     ArmDriver(int argc, char **argv, std::string node_name);
-    /**
-     * Adds an exclamation point to a given string
-     *
-     * Some Longer explanation should go here
-     *
-     * @param input_string the string to add an exclamation point to
-     *
-     * @return input_string with an exclamation point added to it
-     */
-     static std::string addCharacterToString(std::string input_string, std::string suffix);
-     std::string suffix;
 
 private:
-    /**
-     * Callback function for when a new string is received
-     *
-     * @param msg the string received in the callback
-     */
-    void subscriberCallBack(const std_msgs::String::ConstPtr& msg);
     /**
      * Callback function for switching between the Xbox and the Pro Controller
      *
@@ -52,12 +35,6 @@ private:
      * false when the Pro Controller mode is active
      */
     void controllerModeCallBack(const std_msgs::Bool::ConstPtr& msg);
-    /**
-     * Publishes a given string
-     *
-     * @param msg_to_publish the string to publish
-     */
-    void republishMsg(std::string msg_to_publish);
 
     // The SerialStream to/from the teensy
     LibSerial::SerialStream teensy;
@@ -70,6 +47,5 @@ private:
     bool xbox_mode = false;
 
     ros::Subscriber my_subscriber;
-    ros::Publisher my_publisher;
 };
 #endif //DRIVERS_ARM_DRIVER_H
--- src/drivers/src/ArmDriver.cpp
+++ src/drivers/src/ArmDriver.cpp
@@ -14,11 +14,6 @@ ArmDriver::ArmDriver(int argc, char **argv, std::string node_name) {
     ros::NodeHandle nh;
     ros::NodeHandle private_nh("~");
 
-    // Obtains character from the parameter server (or launch file), sets '!' as default
-    std::string parameter_name    = "character";
-    std::string default_character = "!";
-    SB_getParam(private_nh, parameter_name, suffix, default_character);
-
     // Get Params
     SB_getParam(private_nh, "port", port, (std::string) "/dev/ttyACM0");
     // Open the given serial port
@@ -27,26 +22,9 @@ ArmDriver::ArmDriver(int argc, char **argv, std::string node_name) {
     teensy.SetCharSize(LibSerial::SerialStreamBuf::CHAR_SIZE_8); // adjust
 
     // Setup Subscriber(s)
-    std::string topic_to_subscribe_to = "subscribe_topic";
-    int queue_size                    = 10;
-    my_subscriber                     = nh.subscribe(
-    topic_to_subscribe_to, queue_size, &ArmDriver::subscriberCallBack, this);
-
     int queue_size                    = 10;
     my_subscriber                     = nh.subscribe(
     "xbox_mode", queue_size, &ArmDriver::controllerModeCallBack, this);
-
-    // Setup Publisher(s)
-    std::string topic = private_nh.resolveName("publish_topic");
-    queue_size        = 1;
-    my_publisher = private_nh.advertise<std_msgs::String>(topic, queue_size);
-}
-
-void ArmDriver::subscriberCallBack(const std_msgs::String::ConstPtr& msg) {
-    ROS_INFO("Received message");
-    std::string input_string = msg->data.c_str();
-    std::string new_msg = addCharacterToString(input_string, suffix);
-    republishMsg(new_msg);
 }
 
 void ArmDriver::controllerModeCallBack(const std_msgs::Bool::ConstPtr& msg) {
@@ -55,15 +33,4 @@ void ArmDriver::controllerModeCallBack(const std_msgs::Bool::ConstPtr& msg) {
         ROS_INFO("Enabling Xbox Mode");
     else
         ROS_INFO("Enabling Pro Controller Mode");
-}
-
-std::string ArmDriver::addCharacterToString(std::string input_string, std::string suffix) {
-    return input_string.append(suffix);
-}
-
-void ArmDriver::republishMsg(std::string msg_to_publish) {
-    std_msgs::String string_to_publish;
-    string_to_publish.data = msg_to_publish;
-    my_publisher.publish(string_to_publish);
-    ROS_INFO("Published message");
 }
