4b9cbd78e531d9e47242508f5f2517a6f46f7f50
Tate Kolton <tatekolton@gmail.com>
2022-07-04 18:16:18 -0700

    Added publisher side for teensy interface with arm. Cartesian mode will require further adjustments to recieve commands from moveit node

--- src/procontroller_snowbots/src/ProController.cpp
+++ src/procontroller_snowbots/src/ProController.cpp
@@ -210,7 +210,19 @@ void ProController::leftJoystickX(int value) {
         // 128 is the center, so this normalizes the result to
         // [-1,1]*Z_SENSITIVITY
         ROS_INFO("Left Joystick X event with value: %d\n", value);
-        x = (value - 128) / 128.0 * X_SENSITIVITY;
+        z = -(value - 128) / 128.0 * Z_SENSITIVITY;
+
+        // Left joystick is only used in x direction in arm joint space mode. Drilling / cartesion mode do not use this joystick
+        if(state == Mode::arm_joint_space) {
+
+            if(z > 0) {
+             armOutVal = leftJSL;
+            }
+
+            else {
+             armOutVal = leftJSR;
+            }
+        }
     }
 }
 
@@ -455,7 +467,6 @@ void ProController::arrowsUorD(int value) {
     } else {
         ROS_INFO("Down button pressed");
         armOutVal = arrowD;
-	if (state == Mode::wheels) {
 	    speed = speed > 5 ? speed - 5 : speed;
 	    ROS_INFO("Speed increased to %d%% of max output", speed);
 	}
