8cae0053434e0c5cecae02f09fe89ff3bb980d84
Ihsan Olawale <ihsanolawale@outlook.com>
2022-07-19 18:53:35 -0700

    Draft implementation of marker detection node

--- src/marker_qr_detection/src/DetectMarker.cpp
+++ src/marker_qr_detection/src/DetectMarker.cpp
@@ -14,10 +14,17 @@ DetectMarker::DetectMarker(int argc, char **argv, std::string node_name) {
     ros::NodeHandle nh;
     ros::NodeHandle private_nh("~");
 
+    // Setup image transport
+    image_transport::ImageTransport it(nh);
+
+    // Initialize ArUco parameters
+    dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_4X4_50);
+    parameters = cv::aruco::DetectorParameters::create();
+
     // Setup Subscriber(s)
     std::string topic_to_subscribe_to = "subscribe_topic";
     int queue_size                    = 10;
-    my_subscriber                     = nh.subscribe(
+    my_subscriber                     = it.subscribe(
     topic_to_subscribe_to, queue_size, &DetectMarker::subscriberCallBack, this);
 
     // Setup Publisher(s)
@@ -26,7 +33,30 @@ DetectMarker::DetectMarker(int argc, char **argv, std::string node_name) {
     my_publisher = private_nh.advertise<std_msgs::String>(topic, queue_size);
 }
 
-void DetectMarker::subscriberCallBack(const std_msgs::String::ConstPtr& msg) {
+void DetectMarker::subscriberCallBack(const sensor_msgs::Image::ConstPtr& msg) {
     ROS_INFO("Received message");
-    std::string input_string = msg->data.c_str();
+    std::vector<int> markerIds = fetchMarkerIds(rosToMat(msg));
+    std::cout << "Markers detected:";
+    for (int markerId: markerIds) {
+        std::cout << " " << markerId;
+    }
+    std::cout << std::endl;
+}
+
+std::vector<int> DetectMarker::fetchMarkerIds(const cv::Mat& image, bool draw_markers) {
+    std::vector<int> markerIds;
+    std::vector<std::vector<cv::Point2f>> markerCorners;
+    cv::aruco::detectMarkers(image, dictionary, markerCorners, markerIds, parameters);
+    if (draw_markers) {
+        cv::Mat outputImage;
+        image.copyTo(outputImage);
+        cv::aruco::drawDetectedMarkers(outputImage, markerCorners, markerIds);
+    }
+    return markerIds;
+}
+
+cv::Mat DetectMarker::rosToMat(const sensor_msgs::Image::ConstPtr& image) {
+    cv_bridge::CvImagePtr image_ptr;
+    image_ptr = cv_bridge::toCvCopy(image, image->encoding);
+    return image_ptr->image;
 }
