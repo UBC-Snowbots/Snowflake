ec3b670bd325e729a9336ca7137520c5c22dbaa6
Ihsan Olawale <ihsanolawale@outlook.com>
2022-07-26 16:37:55 -0700

    Replace melodic with noetic and bionic with focal

--- setup_scripts/install_dependencies.sh
+++ setup_scripts/install_dependencies.sh
@@ -18,11 +18,11 @@ echo "Installing ROS Melodic"
 echo "================================================================"
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt-get update -y
 
-sudo apt-get install python-catkin-pkg python-wstool python-rosdep python-rosinstall-generator ros-melodic-desktop-full -y
-source /opt/ros/melodic/setup.sh
+sudo apt-get install python3-catkin-pkg python3-wstool python3-rosdep python3-rosinstall-generator ros-noetic-desktop-full -y
+source /opt/ros/noetic/setup.sh
 
 # Prepare resdep to install dependencies
 sudo rosdep init
@@ -51,7 +51,7 @@ cd $CURR_DIR
 rosdep install --from-paths \
     $CURR_DIR/../src \
     $CURR_DIR/../src/external_pkgs \
-    --ignore-src --rosdistro melodic --skip-keys=librealsense2 -y 
+    --ignore-src --rosdistro noetic --skip-keys=librealsense2 -y
 
 echo "================================================================"
 echo "Installing other dependencies specified by our packages"
