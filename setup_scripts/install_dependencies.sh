2b1e48ff4ba52b67a9deb0e5bcf71fe6295003bb
Ihsan Olawale <ihsanolawale@outlook.com>
2022-07-30 07:50:45 -0700

    Change curl to wget

--- setup_scripts/install_dependencies.sh
+++ setup_scripts/install_dependencies.sh
@@ -18,7 +18,7 @@ echo "Installing ROS Melodic"
 echo "================================================================"
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc -O - | sudo apt-key add -
 sudo apt-get update -y
 
 sudo apt-get install python3-catkin-pkg python3-wstool python3-rosdep python3-rosinstall-generator ros-noetic-desktop-full -y
