971fcfaea0f92da73f65073c6ea95ecae9eeef0e
Ihsan Olawale <ihsanolawale@outlook.com>
2022-07-31 10:31:26 -0700

    Add -y option to add-apt-repository

--- setup_scripts/install_dependencies.sh
+++ setup_scripts/install_dependencies.sh
@@ -18,7 +18,7 @@ echo "Installing ROS Noetic"
 echo "================================================================"
 
 sudo apt-get install -y software-properties-common
-sudo add-apt-repository ppa:rock-core/qt4
+sudo add-apt-repository ppa:rock-core/qt4 -y
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc -O - | sudo apt-key add -
