88c0b8f3c8331076aedc9f1c1c9b8e1b9dcedfd5
adam nguyen <avnguyen213@gmail.com>
2022-07-31 17:08:57 -0700

    moved ros package global installs to package.xml

--- setup_scripts/snowbots_arm_deps/install-snowbots-arm-dep.sh
+++ setup_scripts/snowbots_arm_deps/install-snowbots-arm-dep.sh
@@ -2,17 +2,7 @@
 
 echo "Installing dependencies for snowbots arm package"
 
-sudo apt-get update && apt-get install -y build-essential \
-	rviz python-rosdep \
-        ros-melodic-joy ros-melodic-joint-state-publisher vim \
-        ros-melodic-robot-state-publisher \
-	ros-melodic-joystick-drivers ros-melodic-joy \
-        ros-melodic-ros-control ros-melodic-ros-controllers \
-        ros-melodic-urdf-tutorial ros-melodic-rqt
-
 echo "Installing ProController moveit driver"
-
-sudo ./pro_controller_moveit_fix.sh
 
 sudo apt-get update && apt-get install -y git dkms
 sudo git clone https://github.com/nicman23/dkms-hid-nintendo && \
