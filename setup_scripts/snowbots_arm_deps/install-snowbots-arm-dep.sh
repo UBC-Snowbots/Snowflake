ade788d368267a8ee21159f2b31a9f518573bd4b
adam nguyen <avnguyen213@gmail.com>
2022-07-31 17:00:02 -0700

    squashed two scripts together

--- setup_scripts/snowbots_arm_deps/install-snowbots-arm-dep.sh
+++ setup_scripts/snowbots_arm_deps/install-snowbots-arm-dep.sh
@@ -32,4 +32,7 @@ sudo cmake . && make install && systemctl enable --now joycond
 
 sudo ./pro_controller_moveit_fix.sh
 
+sudo cp moveitjoy_module.py /opt/ros/melodic/lib/python2.7/dist-packages/moveit_ros_visualization
+
+echo "ProController is now enabled"
 
--- setup_scripts/snowbots_arm_deps/pro_controller_moveit_fix.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cp moveitjoy_module.py /opt/ros/melodic/lib/python2.7/dist-packages/moveit_ros_visualization
-
-echo "ProController is now enabled"
-
-
