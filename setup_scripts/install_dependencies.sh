3c82fae3f65f688b3fdee9dd49176c4a92b01845
Ihsan Olawale <43079005+ihsan314@users.noreply.github.com>
2022-08-05 17:52:47 -0700

    Fix installation scripts (#420)
    * Fix install script
    
    * Uncomment CMake installation
    
    * Uncomment phidgets and realsense installations
    
    Co-authored-by: TateKolton <tatekolton@gmail.com>

--- setup_scripts/install_dependencies.sh
+++ setup_scripts/install_dependencies.sh
@@ -75,28 +75,26 @@ echo "Installing Robotic Arm Dependencies"
 echo "================================================================"
 
 echo "Upgrading CMake"
-apt-get install wget
-apt-get install apt-transport-https ca-certificates gnupg software-properties-common wget -y
+sudo apt-get install wget -y
+sudo apt-get install apt-transport-https ca-certificates gnupg software-properties-common wget -y
 wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | sudo apt-key add
-apt-add-repository 'deb https://apt.kitware.com/ubuntu/ bionic main'
-apt-get update
-apt-get install cmake
+sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ bionic main'
+sudo apt-get update
+sudo apt-get install cmake -y
 
 echo "Installing Nintendo HID kernel module"
 
-apt-get update && apt-get install -y git dkms
-git clone https://github.com/nicman23/dkms-hid-nintendo && \
-cd ./dkms-hid-nintendo && \
-add . && sudo dkms build nintendo -v 3.2 && sudo dkms install nintendo -v 3.2
+sudo apt-get update && sudo apt-get install -y git dkms
+cd $CURR_DIR/../external_libs/dkms-hid-nintendo && \
+sudo dkms add . && sudo dkms build nintendo -v 3.2 && sudo dkms install nintendo -v 3.2
 
 echo "the Joycond Linux Daemon"
 
-apt-get install libudev-dev -y
-git clone https://github.com/DanielOgorchock/joycond && \
-cd ../joycond && \
-cmake . && make install && systemctl enable --now joycond
+sudo apt-get install libevdev-dev libudev-dev -y
+cd $CURR_DIR/../external_libs/joycond && \
+cmake . && sudo make install && sudo systemctl enable --now joycond
 
-sudo cp moveitjoy_module.py /opt/ros/melodic/lib/python2.7/dist-packages/moveit_ros_visualization
+sudo cp $CURR_DIR/moveitjoy_module.py /opt/ros/melodic/lib/python2.7/dist-packages/moveit_ros_visualization
 
 echo "ProController is now enabled"
 
