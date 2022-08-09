3de521ea60c11b385602277c32069da51d0740e1
Roozki <zawadzkirowan@gmail.com>
2022-08-08 23:07:30 -0700

    fixed error in install_libspb, corrected a change directory

--- setup_scripts/install_libsbp.sh
+++ setup_scripts/install_libsbp.sh
@@ -4,7 +4,7 @@ echo "================================================================"
 echo "Installing Swift Navigation's Binary Protocol library || (GPS)"
 echo "================================================================"
 
-cd external_libs/libsbp/c # get to build space
+cd ../external_libs/libsbp/c # get to build space
 mkdir build
 cd build
 cmake ../
