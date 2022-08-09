fde6db55b63e7cdf6460bb7fd3137eacec85a877
Roozki <zawadzkirowan@gmail.com>
2022-08-09 03:47:00 -0700

    added functionality of saving photos from eiter camera

--- src/cameras/src/ImageSaver.cpp
+++ src/cameras/src/ImageSaver.cpp
@@ -1,7 +1,7 @@
 /*
  * Created By: Rowan Zawadzki
  * Created On: Aug 8 2022
- * Description: A node that saves images
+ * Description: A node that saves images from the realsense cameras CIRC2022
  */
 
 #include <ImageSaver.h>
@@ -13,42 +13,84 @@ ImageSaver::ImageSaver(int argc, char **argv, std::string node_name) {
     ros::NodeHandle nh;
     ros::NodeHandle private_nh("~");
     image_transport::ImageTransport it(nh);
-    // Obtains character from the parameter server (or launch file), sets '!' as default
-    //std::string parameter_name    = "character";
-   // SB_getParam(private_nh, parameter_name, suffix, default_character);
-
+    //decide which camera to save from
+//std::string parameter1    = "camera";
+   // SB_getParam(private_nh, parameter1, camera, 2);
     // Setup Subscriber(s)
-    std::string topic_to_subscribe_to = "cam_1/color/image_raw";
+    std::string cam1_topic;
+   std::string cam2_topic;
+    cam1_topic = "cam_1/color/image_raw";
+   cam2_topic = "cam_2/color/image_raw";
+
+       std::string shutter1_topic = "shutter1_button";
+      std::string shutter2_topic = "shutter2_button";
+
+
+    
     int queue_size                    = 10;
-    camera_subscribe                  = it.subscribe(topic_to_subscribe_to, queue_size, &ImageSaver::subscriberCallBack, this);
-    
-    std::string topic_to_subscribe_to2 = "shutter_button";
-    int queue_size2                    = 2;
-    shutter                  = nh.subscribe(topic_to_subscribe_to2, queue_size2, &ImageSaver::subscriberCallBack2, this);
+    camera1_subscribe                  = it.subscribe(cam1_topic, queue_size, &ImageSaver::subscriberCallBack1, this);
+        camera2_subscribe                  = it.subscribe(cam2_topic, queue_size, &ImageSaver::subscriberCallBack2, this);
+
+    int queue_size2                    = 4;
+    shutter1                  = nh.subscribe(shutter1_topic, queue_size2, &ImageSaver::subscriberCallBackShutter1, this);
+    shutter2                  = nh.subscribe(shutter2_topic, queue_size2, &ImageSaver::subscriberCallBackShutter2, this);
 
 
     
-    // Setup Publisher(s)
-    //to automatically take photos
-    //std::string topic = private_nh.resolveName("shutter_button");
-   // queue_size        = 1;
-   // test = private_nh.advertise<std_msgs::Int32>(topic, queue_size);
-    //ROS_INFO("Virtual button has been pushed, taking photo(s) and saving to");
+    
+    
 }
 
-void ImageSaver::subscriberCallBack(const sensor_msgs::Image::ConstPtr& img) {
-    if (PicsToTake > 0){
+void ImageSaver::subscriberCallBack1(const sensor_msgs::Image::ConstPtr& img1) {
+    if (PicsToTake1 > 0){
+   
+
+  camera = 1;
+   
+      
+
       cv::Mat image;
-      image = cv_bridge::toCvShare(img, "rgb8")->image;
-      cv::imwrite("uniqueName", image);
+      image = cv_bridge::toCvShare(img1, "bgr8")->image;
+      
+       ROS_INFO("Camera %d shutter has been pushed ---%d--- time(s) so far, taking photo(s) and saving to your Snowflake/docs/CIRC_Images/cam%d--images file.", camera, picCounter1 + 1, camera);
+
+      cv::imwrite(cv::format("../Snowflake/docs/CIRC_Images/cam1--images/%d.png", picCounter1), image);
+      picCounter1 ++;
+      PicsToTake1 -= 1;void subscriberCallBack(const sensor_msgs::Image::ConstPtr& image);
+      
+    }
+    
+   
+}
+void ImageSaver::subscriberCallBack2(const sensor_msgs::Image::ConstPtr& img2) {
+    if (PicsToTake2 > 0){
+   
+
+      camera = 2;
+   
+      
+
+      cv::Mat image;
+      image = cv_bridge::toCvShare(img2, "bgr8")->image;
+    
+       ROS_INFO("Camera %d shutter has been pushed ---%d--- time(s) so far, taking photo(s) and saving to your Snowflake/docs/CIRC_Images/cam%d--images file.", camera, picCounter2 + 1, camera);
+
+      cv::imwrite(cv::format("../Snowflake/docs/CIRC_Images/cam2--images/%d.png", picCounter2), image);
+      picCounter2 ++;
+      PicsToTake2 -= 1;
+      
 
     }
    
 }
+void ImageSaver::subscriberCallBackShutter1(const std_msgs::Int32::ConstPtr& msg) {
 
-void ImageSaver::subscriberCallBack2(const std_msgs::Int32::ConstPtr& msg) {
+   PicsToTake1 += msg->data; 
+   
+}
+void ImageSaver::subscriberCallBackShutter2(const std_msgs::Int32::ConstPtr& msg) {
 
-   PicsToTake += msg->data; 
+   PicsToTake2 += msg->data; 
    
 }
 
